7d9e19a3b469d1e6748b18c57d9c6b4a8d90b2d9
qsmith <qsmith@gmail.com>
2024-05-03 22:43:52 +0000

    Prettier

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -1,14 +1,14 @@
-import { initClient } from '@ts-rest/core';
-import { contract } from './contract';
+import { initClient } from "@ts-rest/core";
+import { contract } from "./contract";
 
 export function newClient(baseUrl: string, token: string) {
-    let baseHeaders: Record<string, string> = {};
-    if (token) {
-        baseHeaders['Authorization'] = "bearer " + token;
-    }
-    return initClient(contract, {
-        baseUrl: baseUrl + "/api",
-        baseHeaders,
-        // Uses `tsRestFetchApi` by default
-    });
+  let baseHeaders: Record<string, string> = {};
+  if (token) {
+    baseHeaders["Authorization"] = "bearer " + token;
+  }
+  return initClient(contract, {
+    baseUrl: baseUrl + "/api",
+    baseHeaders,
+    // Uses `tsRestFetchApi` by default
+  });
 }
