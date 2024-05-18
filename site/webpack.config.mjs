58cbc78fc99460983146fa7e80082817000c64d0
Drew Fisher <drew.m.fisher@gmail.com>
2024-05-17 21:10:21 -0700

    Dedupe pngrule again

--- site/webpack.config.mjs
+++ site/webpack.config.mjs
@@ -60,14 +60,15 @@ export default function createConfigs(_env, argv) {
     ],
   };
 
-  const pngRule = {
+  const pngRule = (publicPath) => ({
     test: /\.png$/,
     type: "asset/resource",
     generator: {
       outputPath: "assets/",
+      publicPath: `${publicPath}/assets/`,
       filename: "[hash][ext][query]",
     },
-  };
+  });
 
   const serverSwcLoader = {
     // .swcrc can be used to configure swc
@@ -109,7 +110,7 @@ export default function createConfigs(_env, argv) {
         cssRule,
         // TODO: support importing other kinds of assets, and aliases for
         // the results of the browser build bundles
-        pngRule,
+        pngRule(""),
       ],
       // Add modules as appropriate
     },
@@ -175,15 +176,7 @@ export default function createConfigs(_env, argv) {
           use: ["swc-loader"],
         },
         cssRule,
-        {
-          // Collect client-referenced assets separately
-          test: /\.png$/,
-          type: "asset/resource",
-          generator: {
-            //outputPath: "assets/"
-            filename: "[hash][ext][query]",
-          },
-        },
+        pngRule("/client"),
       ],
     },
     resolve: {
