1cf076309ca9f76f8bbf67999effa3517bb5eee3
Drew Fisher <drew.m.fisher@gmail.com>
2024-05-22 15:32:00 -0700

    npm run format

--- site/tests/login.spec.ts
+++ site/tests/login.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('can log in', async ({ page }) => {
-    await page.goto('/');
+test("can log in", async ({ page }) => {
+  await page.goto("/");
 
-    await page.getByLabel('Username:').fill('team');
-    await page.getByLabel('Password:').fill('password');
-    await page.getByRole('button', { name: /Log in/i }).click();
+  await page.getByLabel("Username:").fill("team");
+  await page.getByLabel("Password:").fill("password");
+  await page.getByRole("button", { name: /Log in/i }).click();
 
-    await expect(page.locator('h1')).toHaveText('Shadow Diamond investigation');
+  await expect(page.locator("h1")).toHaveText("Shadow Diamond investigation");
 });
