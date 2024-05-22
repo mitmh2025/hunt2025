776454bc1b6bf5393b689891bc0066aed5ccfabe
Quentin Smith <quentin@mit.edu>
2024-05-22 16:13:25 -0400

    Initial Playwright test

---
+++ site/tests/login.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('can log in', async ({ page }) => {
+    await page.goto('/');
+
+    await page.getByLabel('Username:').fill('team');
+    await page.getByLabel('Password:').fill('password');
+    await page.getByRole('button', { name: /Log in/i }).click();
+
+    await expect(page.locator('h1')).toHaveText('Shadow Diamond investigation');
+});
