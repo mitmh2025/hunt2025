77eacb91f8aaa701cbaae59c7056418f88494ed0
Drew Fisher <drew.m.fisher@gmail.com>
2024-05-23 09:53:00 -0700

    Create reuseable RoundPuzzleList component
    This renders the puzzle list based on the team state data provided by
    the backend.

---
+++ site/src/frontend/components/RoundPuzzleList.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import type { TeamState } from "../../../lib/api/client";
+import { PUZZLES } from "../puzzles";
+
+const RoundPuzzleList = ({
+  teamState,
+  round,
+}: {
+  teamState: TeamState;
+  round: string;
+}) => {
+  const rounds = teamState.rounds;
+  const roundState = rounds[round];
+  if (roundState === undefined) {
+    return undefined;
+  }
+  const slots = roundState.slots;
+  const puzzleStates = teamState.puzzles;
+  const entries = Object.keys(slots).map((slot) => {
+    const slug = slots[slot];
+    const puzzleState = slug ? puzzleStates[slug] : undefined;
+    const puzzleDefinition = slug ? PUZZLES[slug] : undefined;
+    const title = puzzleDefinition?.title || `Stub puzzle for slot ${slot}`;
+    let item;
+    if (puzzleState?.locked === "locked") {
+      item = (
+        <span className="puzzle-list-item-unlockable">{title} (locked)</span>
+      );
+    }
+    if (puzzleState?.locked === "unlocked") {
+      item = (
+        <a className="puzzle-list-item-unlocked" href={`/puzzles/${slug}`}>
+          {title}
+        </a>
+      );
+    }
+    if (item) {
+      return <li key={slug}>{item}</li>;
+    } else {
+      return undefined;
+    }
+  });
+  return <ul className="puzzle-list">{entries}</ul>;
+};
+
+export default RoundPuzzleList;
--- site/src/frontend/rounds/shadow_diamond/index.tsx
+++ site/src/frontend/rounds/shadow_diamond/index.tsx
@@ -1,43 +1,23 @@
 import React from "react";
-import type { ReactNode } from "react";
 import type { TeamState } from "../../../../lib/api/client";
 import photoimage from "../../../assets/demo-photo.png";
+import RoundPuzzleList from "../../components/RoundPuzzleList";
 
 const ShadowDiamondRoundPage = ({ teamState }: { teamState: TeamState }) => {
-  // TODO: Make helper functions/components
   const roundState = teamState.rounds["shadow_diamond"];
   if (roundState === undefined) {
     return undefined;
   }
-  const slots = roundState.slots;
-  const puzzleStates = teamState.puzzles;
-  const ifUnlocked = (slot: string, callback: (slug: string) => ReactNode) => {
-    const slug = slots[slot];
-    const puzzleState = slug ? puzzleStates[slug] : undefined;
-    if (slug && puzzleState?.locked === "unlocked") {
-      return callback(slug);
-    }
-    return undefined;
-  };
   return (
     <div>
       <h1>Shadow Diamond investigation</h1>
-      <p>TODO: show puzzle list based on props</p>
-      {ifUnlocked("sdm02", (slug) => (
-        <a href={`/puzzles/${slug}`}>sdm02 is unlocked</a>
-      ))}
-      {ifUnlocked("sdm03", (slug) => (
-        <a href={`/puzzles/${slug}`}>
-          sdm03 (which should be Casino) is unlocked
-        </a>
-      ))}
-      <ul></ul>
       <img
         className="photo"
         src={photoimage}
         alt="Outline of suspect photograph"
       />
       <p>This image was included via SSR asset usage.</p>
+      <RoundPuzzleList teamState={teamState} round="shadow_diamond" />
       <div id="shadow-diamond-root" />
     </div>
   );
