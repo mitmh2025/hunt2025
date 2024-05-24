4ee8801569e99dda7119f2d8bb534ce3049685bf
Quentin Smith <quentin@mit.edu>
2024-05-23 23:06:25 -0400

    GitHub playwright action uses a VM with Postgres

--- infra/test.nix
+++ infra/test.nix
@@ -10,13 +10,9 @@ nixosTest {
       ./services/hunt2025.nix
     ];
     nixpkgs.overlays = [(final: prev: {
-      hunt2025 = prev.hunt2025.overrideAttrs (old: {
-        # Keep dev dependencies so we can use playwright
-        dontNpmPrune = true;
-        postInstall = old.postInstall + ''
-          cp package-lock.json $out/lib/node_modules/hunt2025/
-        '';
-      });
+      hunt2025 = prev.hunt2025.override {
+        withDevDeps = true;
+      };
     })];
     systemd.services.hunt2025.environment.DB_ENV = lib.mkForce "ci";
     environment.systemPackages = [
@@ -24,16 +20,24 @@ nixosTest {
       (pkgs.writeShellScriptBin "hunt2025-playwright" ''
         export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
         export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+        export PLAYWRIGHT_HTML_REPORT=/tmp/playwright-report
+        export PW_TEST_HTML_REPORT_OPEN=never
         set -ex
-        cd ${pkgs.hunt2025}/lib/node_modules/hunt2025
-        exec ${pkgs.nodejs_22}/bin/npx playwright test --output /tmp/test-results
+        cd ${pkgs.hunt2025}/lib/hunt2025
+        exec ${pkgs.nodejs_22}/bin/npx playwright test --output /tmp/test-results --reporter=github,html
       '')
     ];
   };
   testScript = ''
+    print("::endgroup::")
+    print("::group::Run VM")
     server.start()
     server.wait_for_unit("hunt2025.service")
-    server.execute("hunt2025-playwright")
-    server.copy_from_vm("/tmp/test-results", "test-results")
+    server.wait_for_open_port(3000)
+    status, stdout = server.execute("hunt2025-playwright")
+    open(server.out_dir / "status", "w").write(str(status))
+    server.copy_from_vm("/tmp/playwright-report", ".")
+    print("::endgroup::")
+    print(stdout)
   '';
 }
