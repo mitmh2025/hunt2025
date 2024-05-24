c53508aaeb9016e2d1abfa7e9bb88727fc935612
Quentin Smith <quentin@mit.edu>
2024-05-24 13:38:36 -0400

    Test to make sure client components render

--- site/tests/login.spec.ts
+++ site/tests/login.spec.ts
@@ -8,4 +8,8 @@ test("can log in", async ({ page }) => {
   await page.getByRole("button", { name: /Log in/i }).click();
 
   await expect(page.locator("h1")).toHaveText("Shadow Diamond investigation");
+
+  await expect(
+    page.getByText("This portion of the page was rendered by browser code"),
+  ).toBeVisible();
 });
