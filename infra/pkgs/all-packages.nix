a5788cece162ea69adb9b5a5d3110ec79b43e404
Quentin Smith <quentin@mit.edu>
2024-06-04 15:46:49 -0400

    Make liquidsoap build on Darwin

--- infra/pkgs/all-packages.nix
+++ infra/pkgs/all-packages.nix
@@ -3,4 +3,32 @@ final: prev: {
   thingsboard = final.callPackage ./thingsboard.nix {};
 
   hunt2025-vm-test = final.callPackage ../test.nix {};
+
+  liquidsoap = if final.stdenv.isDarwin then (prev.liquidsoap.override (old: {
+    # Remove optional dependencies that don't build on Darwin.
+    runtimePackages = with final; [
+      curl
+      ffmpeg
+      yt-dlp
+    ];
+    ocamlPackages = old.ocamlPackages.overrideScope (ocaml-final: ocaml-prev: {
+      alsa = null;
+      alsa-lib = null;
+      dssi = null; # Requires ALSA
+      inotify = null;
+      ctypes-foreign = ocaml-prev.ctypes-foreign.overrideAttrs {
+        # One test fails with Wincompatible-function-pointer-types
+        doCheck = false;
+      };
+      # Dune tries to compile C++ code with clang (instead of clang++)
+      # https://github.com/ocaml/dune/pull/5185
+      soundtouch = null;
+      taglib = null;
+    });
+  })).overrideAttrs (old: {
+    nativeBuildInputs = with final; old.nativeBuildInputs ++ [
+      # Needs `codesign` to build on Darwin.
+      darwin.sigtool
+    ];
+  }) else prev.liquidsoap;
 }
