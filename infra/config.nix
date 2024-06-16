64769a1a736e48a65cf8de46e29c69d849935151
Quentin Smith <quentin@mit.edu>
2024-06-16 12:59:51 -0400

    Store Terraform state in a bucket

--- infra/config.nix
+++ infra/config.nix
@@ -1,4 +1,6 @@
-{
+let
+  stateBucket = "rb8tcjeo-tfstate";
+in {
   # Nix's OpenTofu packaging uses registry.terraform.io for providers.
   terraform.required_providers.local = {
     source = "registry.terraform.io/hashicorp/local";
@@ -10,6 +12,29 @@
   provider.google = {
     project = "mitmh2025-staging-gcp";
     region = "us-east5"; # Ohio
+  };
+
+  resource.google_storage_bucket.tfstate = {
+    name          = stateBucket;
+    force_destroy = false;
+    public_access_prevention = "enforced"; # Always require credentials
+    location      = "US"; # Multi-region bucket
+    storage_class = "STANDARD";
+    versioning.enabled = true; # Keep old state files
+    lifecycle_rule = [{
+      condition = {
+        # Delete old versions if they are >14d old and there are 10 newer versions.
+        with_state = "ARCHIVED";
+        num_newer_versions = 10;
+        days_since_noncurrent_time = 14;
+      };
+      action.type = "Delete";
+    }];
+  };
+
+  terraform.backend.gcs = {
+    bucket = stateBucket;
+    prefix = "terraform/state";
   };
 
   # $ gcloud kms keyrings create sops --location global
