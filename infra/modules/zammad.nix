b2d13f9a3dfa0018dc77189d9eca31ca0dcacb38
Quentin Smith <quentin@mit.edu>
2024-07-12 02:25:15 -0400

    Configure outbound e-mail for Zammad

--- infra/modules/zammad.nix
+++ infra/modules/zammad.nix
@@ -6,6 +6,8 @@ let
     Settings = lib.mapAttrsToList (name: value: {
       inherit name value;
     }) cfg.settings;
+    Channels = cfg.channels;
+    EmailAddresses = cfg.addresses;
   };
 in {
   disabledModules = [ "services/development/zammad.nix" ];
@@ -15,7 +17,7 @@ in {
     inherit (prev) options;
     config = lib.recursiveUpdate prev.config {
       content.systemd.services.zammad-web.preStart = prev.config.content.systemd.services.zammad-web.preStart + ''
-        cp ${autoWizard} ./auto_wizard.json
+        cp ${config.sops.templates."auto_wizard.json".path} ./auto_wizard.json
         chmod +w ./auto_wizard.json
         ./script/rails zammad:setup:auto_wizard[./auto_wizard.json]
       '';
@@ -23,10 +25,28 @@ in {
   })];
 
   options = with lib; {
-    services.zammad.settings = mkOption {
-      type = with types; attrsOf format.type;
-      default = {};
-      description = "Settings to enforce at Zammad startup";
+    services.zammad = {
+      settings = mkOption {
+        type = types.attrsOf format.type;
+        default = {};
+        description = "Settings to enforce at Zammad startup";
+      };
+      channels = mkOption {
+        type = types.listOf format.type;
+        default = [];
+      };
+      addresses = mkOption {
+        type = types.listOf format.type;
+        default = [];
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    sops.templates."auto_wizard.json" = {
+      group = "zammad";
+      mode = "0440";
+      file = autoWizard;
     };
   };
 }
--- infra/services/zammad.nix
+++ infra/services/zammad.nix
@@ -4,6 +4,8 @@
     sops.secrets."zammad/secret_key_base" = {
       owner = config.systemd.services.zammad-web.serviceConfig.User;
     };
+    sops.secrets."zammad/smtp/user" = {};
+    sops.secrets."zammad/smtp/password" = {};
     services.zammad = {
       enable = true;
       redis.port = 6389;  # Deconflict with Authentik
@@ -16,6 +18,50 @@
         fqdn = "tix.mitmh2025.com";
         http_type = "https";
       };
+
+      channels = let
+        outbound = {
+          adapter = "smtp";
+          options = {
+            host = "smtp.postmarkapp.com";
+            port = 587;
+            user = config.sops.placeholder."zammad/smtp/user";
+            password = config.sops.placeholder."zammad/smtp/password";
+            domain = "mitmh2025.com";
+            enable_starttls_auto = true;
+            ssl_verify = true;
+          };
+        };
+      in [
+        {
+          id = 1;
+          area = "Email::Notification";
+          options.outbound = outbound;
+          preferences.online_service_disable = true;
+        }
+        {
+          id = 2;
+          area = "Email::Account";
+          options.inbound = { adapter = "null"; options = {}; };
+          options.outbound = outbound;
+          active = true;
+        }
+      ];
+
+      addresses = [
+        {
+          id = 1;
+          channel_id = 2;
+          name = "Hunt 2025";
+          email = "help@mitmh2025.com";
+        }
+        {
+          id = 2;
+          channel_id = 2;
+          name = "Hints";
+          email = "hints@mitmh2025.com";
+        }
+      ];
     };
     services.nginx = {
       upstreams.zammad.servers."127.0.0.1:${toString config.services.zammad.port}" = {};
