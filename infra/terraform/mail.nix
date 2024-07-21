3eac9dbc01bc1a49299044cb971c70356516bd22
Evan Broder <evan@ebroder.net>
2024-07-20 22:40:52 -0700

    Add TF configuration for SES

--- infra/terraform/mail.nix
+++ infra/terraform/mail.nix
@@ -38,4 +38,45 @@
       "pm.mtasv.net"
     ];
   };
+  resource.aws_ses_domain_identity.mitmh2025 = {
+    domain = "mitmh2025.com";
+  };
+  resource.aws_route53_record.mitmh2025_ses_verification = {
+    zone_id = lib.tfRef "data.aws_route53_zone.mitmh2025.zone_id";
+    name = "_amazonses.mitmh2025.com";
+    type = "TXT";
+    ttl = "300";
+    records = [
+      (lib.tfRef "aws_ses_domain_identity.mitmh2025.verification_token")
+    ];
+  };
+  resource.aws_ses_domain_mail_from.mitmh2025 = {
+    domain = lib.tfRef "aws_ses_domain_identity.mitmh2025.domain";
+    mail_from_domain = "mail.mitmh2025.com";
+  };
+  resource.aws_route53_record.mitmh2025_ses_mail_from_txt = {
+    zone_id = lib.tfRef "data.aws_route53_zone.mitmh2025.zone_id";
+    name = lib.tfRef "resource.aws_ses_domain_mail_from.mitmh2025.mail_from_domain";
+    type = "TXT";
+    ttl = "300";
+    records = ["v=spf1 include:amazonses.com ~all"];
+  };
+  resource.aws_route53_record.mitmh2025_ses_mail_from_mx = {
+    zone_id = lib.tfRef "data.aws_route53_zone.mitmh2025.zone_id";
+    name = lib.tfRef "resource.aws_ses_domain_mail_from.mitmh2025.mail_from_domain";
+    type = "MX";
+    ttl = "300";
+    records = ["10 feedback-smtp.us-east-1.amazonses.com"];
+  };
+  resource.aws_ses_domain_dkim.mitmh2025 = {
+    domain = lib.tfRef "aws_ses_domain_identity.mitmh2025.domain";
+  };
+  resource.aws_route53_record.mitmh2025_ses_dkim = {
+    count = 3;
+    zone_id = lib.tfRef "data.aws_route53_zone.mitmh2025.zone_id";
+    name = lib.concatStrings [(lib.tfRef "aws_ses_domain_dkim.mitmh2025.dkim_tokens[count.index]") "._domainkey"];
+    type = "CNAME";
+    ttl = "1800";
+    records = [(lib.concatStrings [(lib.tfRef "aws_ses_domain_dkim.mitmh2025.dkim_tokens[count.index]") ".dkim.amazonses.com"])];
+  };
 }
