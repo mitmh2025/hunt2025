ccefea665745c1a61ead98029d450ebb1a7aa641
Drew Fisher <drew.m.fisher@gmail.com>
2024-08-23 17:00:44 -0700

    Rework DB seed to handle canonical data in activity_log
    In particular: it is important that we stop wiping the activity log for
    non-custom-behavior teams.  It's likely we'll drop the `visible` fixture
    entirely soon (as we have no puzzles that should ever be in that state
    -- they should all be either `unlockable` or `unlocked`).

--- site/seeds/demo.ts
+++ site/seeds/demo.ts
@@ -4,25 +4,23 @@ import { PUZZLES } from "../src/frontend/puzzles";
 import HUNT from "../src/huntdata";
 import { getSlugsBySlot } from "../src/huntdata/logic";
 
+const CUSTOM_BEHAVIOR_TEAMS = ["visible", "unlockable", "unlocked", "solved"];
+
 export async function seed(knex: Knex): Promise<void> {
-  // Inserts seed entries
+  const slotsToSlug = getSlugsBySlot(HUNT);
+  const slugs = new Set(Object.values(slotsToSlug));
+
+  // Ensure users exist in the teams table.
   const usernameToTeamId = new Map<string, number>();
-  for (const username of [
-    "team",
-    "visible",
-    "unlockable",
-    "unlocked",
-    "solved",
-  ]) {
+  for (const username of ["team", ...CUSTOM_BEHAVIOR_TEAMS]) {
     await knex("teams")
       .insert([{ username, password: "password" }])
       .onConflict("username")
       .ignore();
   }
-  const slotsToSlug = getSlugsBySlot(HUNT);
-  const slugs = new Set(Object.values(slotsToSlug));
 
-  for (const username of ["visible", "unlockable", "unlocked", "solved"]) {
+  // Prepare username -> id lookup map for later
+  for (const username of CUSTOM_BEHAVIOR_TEAMS) {
     const team = await knex("teams")
       .where("username", username)
       .select("id")
@@ -30,29 +28,23 @@ export async function seed(knex: Knex): Promise<void> {
     if (!team) continue; // guaranteed to be truthy because we just inserted the row above
     const team_id = team.id;
     usernameToTeamId.set(username, team_id);
-    await knex("team_rounds")
-      .insert(
-        HUNT.rounds.map(({ slug }) => ({
-          team_id,
-          slug,
-          unlocked: true, // We don't have a concept of "unlockable" rounds, so just make them all unlocked.
-        })),
-      )
-      .onConflict(["team_id", "slug"])
-      .ignore();
-    await knex("team_puzzles").where("team_id", team_id).del();
-    await knex("team_puzzle_guesses").where("team_id", team_id).del();
-    await knex("team_puzzles").insert(
-      Array.from(slugs).map((slug) => ({
-        team_id,
-        slug,
-        visible: true,
-        unlockable: username !== "visible",
-        unlocked: username === "unlocked" || username === "solved",
-      })),
-    );
   }
 
+  // Wipe the activity log and other canonical stateful tables for the "custom behavior" teams
+  const custom_team_ids = await knex("teams")
+    .whereIn("username", CUSTOM_BEHAVIOR_TEAMS)
+    .select("id")
+    .pluck("id");
+  await knex.transaction(async (trx) => {
+    for (const team_id of custom_team_ids) {
+      await trx("activity_log").where("team_id", team_id).del();
+      await trx("team_puzzle_guesses").where("team_id", team_id).del();
+      await trx("team_puzzles").where("team_id", team_id).del();
+      await trx("team_rounds").where("team_id", team_id).del();
+    }
+  });
+
+  // Create correct guesses for all puzzles from the "solved" team
   await knex("team_puzzle_guesses").insert(
     Array.from(slugs).flatMap((slug) => {
       const puzzle = PUZZLES[slug];
@@ -69,10 +61,40 @@ export async function seed(knex: Knex): Promise<void> {
       }));
     }),
   );
-  const team_ids = await knex("teams").select("id").pluck("id");
+
+  // Backfill non-canonical states for each of the custom view users.  These probably need to go away soon.
+  for (const username of CUSTOM_BEHAVIOR_TEAMS) {
+    const team_id = usernameToTeamId.get(username);
+    if (!team_id) continue; // guaranteed to be populated above
+
+    // Unlock all rounds for all "custom behavior" teams.
+    await knex("team_rounds")
+      .insert(
+        HUNT.rounds.map(({ slug }) => ({
+          team_id,
+          slug,
+          unlocked: true, // We don't have a concept of "unlockable" rounds, so just make them all unlocked.
+        })),
+      )
+      .onConflict(["team_id", "slug"])
+      .ignore();
+
+    // Set puzzle state as appropriate for a given username.
+    await knex("team_puzzles").insert(
+      Array.from(slugs).map((slug) => ({
+        team_id,
+        slug,
+        visible: true,
+        unlockable: username !== "visible",
+        unlocked: username === "unlocked" || username === "solved",
+      })),
+    );
+  }
+
+  // Ensure we trigger any triggerable unlocks
+  const all_team_ids = await knex("teams").select("id").pluck("id");
   await knex.transaction(async (trx) => {
-    for (const team_id of team_ids) {
-      await trx("activity_log").where("team_id", team_id).del();
+    for (const team_id of all_team_ids) {
       await recalculateTeamState(HUNT, team_id, trx);
     }
   });
