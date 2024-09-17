3e9f9c174ed04c709d588dd61fa92ab2846d78c3
Drew Fisher <drew.m.fisher@gmail.com>
2024-09-17 01:58:29 -0700

    Extract DeferredPublication to a separate file
    We'll want to reuse it in the InteractionEngine, eventually.

--- site/src/api/server.ts
+++ site/src/api/server.ts
@@ -14,11 +14,7 @@ import {
 } from "express";
 import jwt from "jsonwebtoken";
 import { type Knex } from "knex";
-import {
-  type GuessStatus,
-  type ActivityLogEntry,
-  type TeamPuzzleGuess,
-} from "knex/types/tables";
+import { type GuessStatus, type ActivityLogEntry } from "knex/types/tables";
 import { Passport } from "passport";
 import { Strategy, ExtractJwt } from "passport-jwt";
 import * as swaggerUi from "swagger-ui-express";
@@ -37,6 +33,7 @@ import { nextAcceptableSubmissionTime } from "../../lib/ratelimit";
 import type { RedisClient } from "../app";
 import { INTERACTIONS } from "../frontend/interactions";
 import { PUZZLES } from "../frontend/puzzles";
+import { DeferredPublications } from "../frontend/server/deferred_publications";
 import { generateSlugToSlotMap } from "../huntdata";
 import { getSlotSlug } from "../huntdata/logic";
 import { type Hunt } from "../huntdata/types";
@@ -113,90 +110,6 @@ function newPassport(jwtSecret: string | Buffer) {
 function canonicalizeInput(input: string) {
   // TODO: Make sure puzzles agrees with this definition.
   return input.replaceAll(/\s+/g, "").toUpperCase();
-}
-
-class DeferredPublications {
-  private activityLog?: ActivityLogEntry[];
-  private guessLog?: TeamPuzzleGuess[];
-  private teamState?: TeamState[];
-
-  constructor({
-    activityLog,
-    guessLog,
-    teamState,
-  }: {
-    activityLog?: ActivityLogEntry[];
-    guessLog?: TeamPuzzleGuess[];
-    teamState?: TeamState[];
-  }) {
-    this.activityLog = activityLog;
-    this.guessLog = guessLog;
-    this.teamState = teamState;
-  }
-
-  addActivityLogEntries(entries: ActivityLogEntry[]) {
-    this.activityLog = (this.activityLog ?? []).concat(entries);
-  }
-
-  addGuessLogEntries(guesses: TeamPuzzleGuess[]) {
-    this.guessLog = (this.guessLog ?? []).concat(guesses);
-  }
-
-  addTeamState(teamStates: TeamState[]) {
-    this.teamState = (this.teamState ?? []).concat(teamStates);
-  }
-
-  concat(other: DeferredPublications) {
-    const activityLog = this.activityLog
-      ? this.activityLog.concat(other.activityLog ?? [])
-      : other.activityLog;
-    const teamState = this.teamState
-      ? this.teamState.concat(other.teamState ?? [])
-      : other.teamState;
-    const guessLog = this.guessLog
-      ? this.guessLog.concat(other.guessLog ?? [])
-      : other.guessLog;
-    return new DeferredPublications({
-      activityLog,
-      guessLog,
-      teamState,
-    });
-  }
-
-  async publish(redisClient?: RedisClient) {
-    if (redisClient) {
-      try {
-        if (this.activityLog) {
-          for (const entry of this.activityLog) {
-            await redisClient.publish(
-              "global/activity_log",
-              JSON.stringify(entry),
-            );
-          }
-        }
-        if (this.teamState) {
-          for (const state of this.teamState) {
-            const team_id = state.teamId;
-            const topic = `team_state.${team_id}`;
-            await redisClient.publish(topic, JSON.stringify(state));
-          }
-        }
-        if (this.guessLog) {
-          for (const guess of this.guessLog) {
-            await redisClient.publish(
-              "global/guess_log",
-              JSON.stringify(guess),
-            );
-          }
-        }
-      } catch (e: unknown) {
-        // Graceful fallback if Redis can't be reached.
-        // It's okay if we drop activity log entries on pubsub; tailers will detect the missing id
-        // and hit the API to get any missing entries
-        console.error("redis publish error", e);
-      }
-    }
-  }
 }
 
 export function getRouter({
