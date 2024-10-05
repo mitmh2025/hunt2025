b9dbaca219a76665eacde551e56c36064d7fd943
Drew Fisher <drew.m.fisher@gmail.com>
2024-10-05 17:14:00 -0400

    Add optional `initial_description` field to PuzzleDefinition
    And add a bunch of comments explaining the other fields.

--- site/src/frontend/puzzles/types.ts
+++ site/src/frontend/puzzles/types.ts
@@ -36,14 +36,49 @@ export type AdditionalCredit = {
 };
 
 export type PuzzleDefinition = {
+  // The title, which should be written in title case
   title: string;
+
+  // The unique slug for this puzzle, which should be approximately the title in all lowercase, with
+  // spaces replaced with underscores.
   slug: string;
+
+  // The non-spoilery description of this puzzle, as seen by hunters while the puzzle is locked but
+  // unlockable (by spending unlock currency).  This should be a description of what hunters will
+  // see on the puzzle page once they unlock the puzzle.
+  initial_description?: string;
+
+  // The canonicalized answer to this puzzle.  It should be written in all caps with appropriate
+  // whitespace.
   answer: string;
+
+  // The list of authors of this puzzle.  Lead author should be first; the other authors should be
+  // sorted alphabetically by lowercased display name.
   authors: string[];
+
+  // The list of editors on this puzzle, sorted by lowercased display name.
   editors: string[];
+
+  // Any additional credits (e.g. tech, art) that we should give for the construction,
+  // implementation, execution, or production of this puzzle.
   additional_credits: AdditionalCredit[];
+
+  // The React component that contains the puzzle content we render in the <div id="puzzle-content">.
+  // It should NOT include a div for the title; it should be purely the puzzle content.
   content: Content;
+
+  // The React component that contains the solution writeup that we render on the solution page.
+  // The actual answer and credits will be generated from the other fields of PuzzleDefinition, so this
+  // should just be the text and images of the steps, and any additional author notes.
   solution: Content;
+
+  // The collection of hints provided by the authors to help hint responders.  These are not
+  // expected to be displayed to solvers directly; they're meant to help guide our operations team.
   hints: Hint[];
+
+  // Canned responses confirming intermediate progress.  When a team submits a guess that matches a
+  // canned response, the `reply` will be shown on the page in response to that guess.
+  // In the future, we may add additional canned response features for e.g. accepting file uploads,
+  // or coordinating times for visiting a particular room.
   canned_responses: CannedResponse[];
 };
