bda00e86f6bd989b4e398a6699e7ef261a239ce5
Drew Fisher <drew.m.fisher@gmail.com>
2024-10-11 02:13:06 -0700

    Add state filters to All Puzzles page
    The hope here is that teams realize they can show just unlockable
    puzzles when trying to figure out which puzzle they want to unlock next.

--- site/src/frontend/client/all_puzzles.tsx
+++ site/src/frontend/client/all_puzzles.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { hydrateRoot } from "react-dom/client";
 import AllPuzzlesList from "../components/AllPuzzlesList";
 import globalDatasetManager from "./DatasetManager";
@@ -10,6 +10,19 @@ const AllPuzzlesManager = ({
   initialState: AllPuzzlesState;
 }) => {
   const [state, setState] = useState<AllPuzzlesState>(initialState);
+  const [showUnlockable, setShowUnlockable] = useState<boolean>(true);
+  const [showUnlocked, setShowUnlocked] = useState<boolean>(true);
+  const [showSolved, setShowSolved] = useState<boolean>(true);
+
+  const toggleShowUnlockable = useCallback(() => {
+    setShowUnlockable((prevState) => !prevState);
+  }, []);
+  const toggleShowUnlocked = useCallback(() => {
+    setShowUnlocked((prevState) => !prevState);
+  }, []);
+  const toggleShowSolved = useCallback(() => {
+    setShowSolved((prevState) => !prevState);
+  }, []);
 
   useEffect(() => {
     const stop = globalDatasetManager.watch(
@@ -22,7 +35,63 @@ const AllPuzzlesManager = ({
     return stop;
   }, []);
 
-  return <AllPuzzlesList state={state} />;
+  const filteredState = useMemo(() => {
+    const rounds = state.rounds.flatMap((round) => {
+      const filteredPuzzles = round.puzzles.filter((p) => {
+        if (p.answer !== undefined) {
+          return showSolved;
+        }
+        if (p.state === "unlocked") {
+          return showUnlocked;
+        }
+        if (p.state === "unlockable") {
+          return showUnlockable;
+        }
+        return true;
+      });
+      return {
+        ...round,
+        puzzles: filteredPuzzles,
+      };
+    });
+    return {
+      ...state,
+      rounds,
+    };
+  }, [state, showUnlockable, showUnlocked, showSolved]);
+
+  return (
+    <>
+      <div>
+        Show:
+        <label>
+          <input
+            type="checkbox"
+            checked={showUnlockable}
+            onChange={toggleShowUnlockable}
+          />
+          unlockable
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={showUnlocked}
+            onChange={toggleShowUnlocked}
+          />
+          unlocked
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={showSolved}
+            onChange={toggleShowSolved}
+          />
+          solved
+        </label>
+      </div>
+      <AllPuzzlesList state={filteredState} />
+    </>
+  );
 };
 
 const allPuzzlesElem = document.getElementById("all-puzzles-root");
