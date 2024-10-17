6cea55d372f22d3a8c55ef07229504b3b3b8fe37
Quentin Smith <quentin@mit.edu>
2024-10-17 03:21:24 -0400

    dev TF config shouldn't create the dev VM

--- infra/terraform/dev/default.nix
+++ infra/terraform/dev/default.nix
@@ -2,40 +2,10 @@
 {
   # Nix's OpenTofu packaging uses registry.terraform.io for providers.
   terraform.required_providers = {
-    aws.source = "registry.terraform.io/hashicorp/aws";
-    google.source = "registry.terraform.io/hashicorp/google";
-    local.source = "registry.terraform.io/hashicorp/local";
     nix.source = "registry.terraform.io/krostar/nix";
-    skopeo2.source = "registry.terraform.io/bsquare-corp/skopeo2";
-    tls.source = "registry.terraform.io/hashicorp/tls";
   };
-
-  provider.google = {
-    project = "mitmh2025-staging-gcp";
-    region = "us-east5"; # Ohio
-    zone = "us-east5-a";
-  };
-
-  provider.aws = [
-    {
-      alias = "puzzup";
-      allowed_account_ids = [891377012427];
-      region = "us-east-1";
-      profile = "mitmh2025-puzzup";
-    }
-  ];
 
   imports = [
     ./dev.nix
   ];
-
-  route53.mitmh2025 = {
-    provider = "puzzup";
-    domain = "mitmh2025.com";
-  };
-
-  terraform.backend.gcs = {
-    bucket = stateBucket;
-    prefix = "terraform/state";
-  };
 }
--- infra/terraform/dev/dev.nix
+++ infra/terraform/dev/dev.nix
@@ -1,20 +1,8 @@
 { lib, self, ... }:
 {
-  gce.instance.dev = {
-    machineType = "e2-micro"; # 1 vCPU, 1 GB RAM
-    firewall.allowedTCPPorts = [
-      22 # SSH
-      80 # HTTP
-      443 # HTTPS
-    ];
-  };
-
   resource.nix_store_path_copy.dev_nixos = {
-    depends_on = [
-      "google_compute_firewall.dev"
-    ];
     store_path = "${self.nixosConfigurations.dev.config.system.build.toplevel}";
-    to = "ssh-ng://root@\${aws_route53_record.dev.fqdn}";
+    to = "ssh-ng://root@dev.mitmh2025.com";
     check_sigs = false;
 
     # TODO: Switch to deploy-rs so we get magic rollback?
@@ -24,20 +12,10 @@
         type = "ssh";
         user = "root";
         agent = true;
-        host = lib.tfRef "aws_route53_record.dev.fqdn";
+        host = "dev.mitmh2025.com";
       };
       inline = [
         "${self.nixosConfigurations.dev.config.system.build.toplevel}/bin/switch-to-configuration switch"
-      ];
-    };
-  };
-
-  route53.mitmh2025.rr = {
-    dev = {
-      type = "A";
-      ttl = "300";
-      records = [
-        (lib.tfRef "resource.google_compute_instance.dev.network_interface.0.access_config.0.nat_ip")
       ];
     };
   };
