030eb2be106c068c0a2a6e2aca63119fdf73d301
Quentin Smith <quentin@mit.edu>
2024-10-17 20:45:42 -0400

    Use common VM config

--- infra/hosts/staging.nix
+++ infra/hosts/staging.nix
@@ -1,7 +1,7 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, pkgs, ... }:
 {
   imports = [
-    "${modulesPath}/virtualisation/google-compute-config.nix"
+    ../services/gce-vm.nix
     ./base.nix
     ../services/postgres.nix
     ../services/redis.nix
@@ -11,36 +11,7 @@
   ];
   config = lib.mkMerge [
     {
-      # Enable compressed RAM
-      boot.kernelParams = ["zswap.enabled=1"];
-
-      # Allow serial console login
-      systemd.services."serial-getty@ttyS0" = {
-        # Undo what nixos/modules/profiles/headless.nix did
-        enable = true;
-        serviceConfig = config.systemd.services."serial-getty@".serviceConfig;
-        overrideStrategy = "asDropin";
-      };
-    }
-    {
-      # Ensure network is available before sops tries to initialize secrets
-      boot.initrd.systemd.enable = true;
-      boot.initrd.network.enable = true;
-      networking.useNetworkd = true;
-      security.polkit.enable = true; # Allows networkd to set hostname
-      boot.initrd.systemd.services.initrd-nixos-activation = let
-        deps = [ "network-online.target" ];
-      in {
-        after = deps;
-        wants = deps;
-      };
-      system.nssDatabases.hosts = lib.mkOrder 401 ["myhostname"]; # Ensure local hostname is not resolved via DNS
-    }
-    {
       sops.defaultSopsFile = ./../secrets/staging.yaml;
-
-      security.acme.acceptTerms = true;
-      security.acme.defaults.email = "hunt2025-tech@googlegroups.com";
     }
     {
       hunt2025.site = {
@@ -62,12 +33,6 @@
     {
       services.thingsboard = {
         enable = true;
-        # logback.loggers = {
-        #   "org.thingsboard.server" = "DEBUG";
-        #   "org.thingsboard.server.actors.TbActorMailbox" = "INFO";
-        #   "org.thingsboard.server.actors.service.ContextAwareActor" = "INFO";
-        # };
-        # logback.rootLevel = "DEBUG";
         datasource.createLocally = true;
       };
     }
