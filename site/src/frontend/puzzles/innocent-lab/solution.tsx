e8b50d7a891eb7a07d4c1dac04519b67b22ccfd5
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2024-11-12 21:56:48 -0500

    i have no idea what i'm doing

--- site/src/frontend/puzzles/innocent-lab/solution.tsx
+++ site/src/frontend/puzzles/innocent-lab/solution.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { styled } from "styled-components";
+import dog from "./assets/dog.png";
 
 const StyledTable = styled.table`
   margin-bottom: 1em;
+  th,
+  td {
+    padding: 0px 8px;
+  }
+`;
+
+const HighlightedTable = styled(StyledTable)`
   border-collapse: collapse;
   tr:nth-child(odd) {
     background-color: #bba5a3;
@@ -22,10 +30,10 @@ const StyledTable = styled.table`
       border: 1px solid #967470;
     }
   }
-  th,
-  td {
-    padding: 0px 8px;
-  }
+`;
+
+const Mono = styled.span`
+  font-family: monospace;
 `;
 
 const Solution = (): JSX.Element => {
@@ -60,7 +68,7 @@ const Solution = (): JSX.Element => {
         compound is correct once it’s been ID’d via wordplay clues.
       </p>
       <p>The table below explains the wordplay and warnings.</p>
-      <StyledTable>
+      <HighlightedTable>
         <tr>
           <th>Compound</th>
           <th>Column 1 (formula)</th>
@@ -271,7 +279,201 @@ const Solution = (): JSX.Element => {
             spontaneously dimerizes into B<sub>2</sub>H<sub>6</sub>
           </td>
         </tr>
+      </HighlightedTable>
+      <p>
+        Once this is done, each set of clues will specifically identify one
+        chemical and provide three different numerical shifts. The aha here is
+        that all chemicals are uniquely associated with a CAS registry number,
+        which has the format of three numbers separated by dashes (for instance,
+        ozone is 10028-15-6). These can be looked up most easily from googling
+        the chemical, as most are listed on wikipedia pages for the compound or
+        on chemical supplier pages. They can also be looked up at
+        <a
+          href="https://commonchemistry.cas.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://commonchemistry.cas.org/
+        </a>
+        . Reverse searching can generally be done fairly easily by googling the
+        CAS number and “cas” together.
+      </p>
+      <p>
+        The three shifts in order can be applied to the CAS number of the
+        compound to make a new CAS number for a compound resembling the original
+        one, except with a new element replacing an atom in the original.
+        Reading the elemental abbreviations of the added atoms, in order, spells
+        out C Al Li N Es Ca La T I O N, or <Mono>CALL IN ESCALATION</Mono>. The
+        answer is thus{" "}
+        <Mono>
+          <strong>ESCALATION</strong>
+        </Mono>
+        .
+      </p>
+      <StyledTable>
+        <tr>
+          <th>Compound</th>
+          <th>CAS</th>
+          <th>Shifts</th>
+          <th>New CAS</th>
+          <th>New compound</th>
+        </tr>
+        <tr>
+          <td>
+            Ozone, O<sub>3</sub>
+          </td>
+          <td>10028-15-6</td>
+          <td>-9904, +23, +3</td>
+          <td>124-38-9</td>
+          <td>
+            Carbon dioxide, CO<sub>2</sub>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Ammonia, NH<sub>3</sub>
+          </td>
+          <td>7664-41-7</td>
+          <td>+120, -20, -1</td>
+          <td>7784-21-6</td>
+          <td>
+            Aluminum hydride, AlH<sub>3</sub>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Methanol, CH<sub>3</sub>OH
+          </td>
+          <td>67-56-1</td>
+          <td>+798, -22, +8</td>
+          <td>865-34-9</td>
+          <td>
+            Lithium methoxide
+            <sup>
+              <a href="#footnote-1">1</a>
+            </sup>
+            , CH<sub>3</sub>OLi
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Arsole, C<sub>4</sub>H<sub>4</sub>AsH
+          </td>
+          <td>287-77-4</td>
+          <td>-178, +20, +3</td>
+          <td>109-97-7</td>
+          <td>
+            Pyrrole, C<sub>4</sub>H<sub>4</sub>NH
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Alumina, Al<sub>2</sub>O<sub>3</sub>
+          </td>
+          <td>1344-28-1</td>
+          <td>+36018, +66, -1</td>
+          <td>37362-94-0</td>
+          <td>
+            Einsteinium oxide, Es<sub>2</sub>O<sub>3</sub>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Milk of magnesia, Mg(OH)<sub>2</sub>
+          </td>
+          <td>1309-42-8</td>
+          <td>-4, +20, -8</td>
+          <td>1305-62-0</td>
+          <td>
+            Calcium hydroxide, Ca(OH)<sub>2</sub>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            iron(III) oxide, Fe<sub>2</sub>O<sub>3</sub>
+          </td>
+          <td>1309-37-1</td>
+          <td>+3, +44, +7</td>
+          <td>1312-81-8</td>
+          <td>
+            Lanthanum sesquioxide, La<sub>2</sub>O<sub>3</sub>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Water, H<sub>2</sub>O
+          </td>
+          <td>7732-18-5</td>
+          <td>+5938, -1, -3</td>
+          <td>13670-17-2</td>
+          <td>
+            HTO (tritium-labeled water)
+            <sup>
+              <a href="#footnote-2">2</a>
+            </sup>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Benzene, C<sub>6</sub>H<sub>6</sub>
+          </td>
+          <td>71-43-2</td>
+          <td>+520, +7, +2</td>
+          <td>591-50-4</td>
+          <td>
+            Iodobenzene, C<sub>6</sub>H<sub>5</sub>I
+          </td>
+        </tr>
+        <tr>
+          <td>Quicklime, CaO</td>
+          <td>1305-78-8</td>
+          <td>+6477, -34, -1</td>
+          <td>7782-44-7</td>
+          <td>
+            Dioxygen, O<sub>2</sub>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Borane, BH<sub>3</sub>
+          </td>
+          <td>13283-31-3</td>
+          <td>-5619, +10, +4</td>
+          <td>7664-41-7</td>
+          <td>
+            Ammonia, NH<sub>3</sub>
+          </td>
+        </tr>
       </StyledTable>
+      <p id="footnote-1">
+        Unfortunately,{" "}
+        <a
+          href="https://commonchemistry.cas.org/detail?cas_rn=865-34-9"
+          target="_blank"
+          rel="noreferrer"
+        >
+          commonchemistry.cas.org
+        </a>{" "}
+        (incorrectly) gives the formula for lithium methoxide as CH<sub>4</sub>
+        OLi (there are historical reasons for this whose discussion is beyond
+        the scope of this solutions page). Hopefully, solvers (if they don’t
+        recognize the error based on chemical knowledge) will notice that the
+        correct formula CH<sub>3</sub>OLi is given by all other readily
+        available sources.
+      </p>
+      <p id="footnote-2">
+        Some sources (incorrectly) give the formula for this CAS number as T2O,
+        but this doesn’t affect the final extraction.
+      </p>
+      <p>
+        Fun fact: in development, this puzzle was assigned the random codename
+        innocent-lab, which led to the following image being adopted as the
+        puzzle mascot:
+      </p>
+      <img
+        src={dog}
+        alt="I Have No Idea What I'm Doing image macro with a Laborador retriever wearing safety glasses in front of a bunch of chemistry equipment filled with colored liquids. The dog is pouring a beaker into a coffee mug."
+      />
     </>
   );
 };
