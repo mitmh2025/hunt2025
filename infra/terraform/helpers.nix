0c8244d576ddcf2255b20fc5158b8e5f1d71ced9
Quentin Smith <quentin@mit.edu>
2024-11-13 19:42:07 -0500

    Make Atlantis able to run plans

--- infra/terraform/helpers.nix
+++ infra/terraform/helpers.nix
@@ -1,6 +1,6 @@
 pkgs: self: super: {
   types = super.types // {
     tfName = self.types.strMatching "^[a-zA-Z_][a-zA-Z0-9_-]*$";
-    tfAttrsOf = arg: self.types.addCheck (self.types.attrsOf arg) (x: self.all self.tfName.check (self.attrNames x));
+    tfAttrsOf = arg: self.types.addCheck (self.types.attrsOf arg) (x: self.all self.types.tfName.check (self.attrNames x));
   };
 }
