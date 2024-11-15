d3999c0a680affff1b2faec02d7fb257ad4edc47
Quentin Smith <quentin@mit.edu>
2024-11-15 13:26:22 -0500

    Give deploy a faster disk

--- infra/terraform/prod/deploy.nix
+++ infra/terraform/prod/deploy.nix
@@ -2,6 +2,7 @@
 {
   # The Cloud Build API uses the consumer project, not the resource project, so we have to enable it here.
   gcp.services.cloudbuild.enable = true;
+  resource.google_compute_disk.deploy.type = lib.mkForce "pd-ssd";
   gce.instance.deploy = {
     route53.zone = "mitmh2025";
     route53.aliases = [
