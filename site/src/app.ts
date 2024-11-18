mitmh2025/hunt2025

commit 5f418c93ad855f9277cf60dcc1de2f66421c99ac

Package assets separately in preparation for CDN deployment

site/src/app.ts
@@ -8,6 +8,7 @@ import { connect as redisConnect } from "./api/redis";
 import { getRouter } from "./api/server";
 import { getUiRouter } from "./frontend/server/routes";
 import HUNT from "./huntdata";
+import { existsSync } from "fs";
 
 const LOG_FORMAT_DEBUG =
   ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" ":req[Authorization]"';
@@ -52,8 +53,11 @@ export default async function ({
   });
   app.use("/api", apiRouter);
 
-  // Serve static assets from the bundle without auth
-  app.use("/static", express.static(path.join(__dirname, "static")));
+  const staticPath = process.env.STATIC_PATH ?? path.join(__dirname, "static");
+  if (existsSync(staticPath)) {
+    // Serve static assets from the bundle without auth
+    app.use("/static", express.static(staticPath));
+  }
 
   // Forward all other requests to the UI router, which we expect to
   // handle most user requests.

infra/hosts/staging/staging.nix
@@ -95,7 +95,7 @@
               # Copy Authentik configuration
               extraConfig = config.services.nginx.virtualHosts."staging.mitmh2025.com".locations."/".extraConfig;
             };
-            locations."/static/".alias = "${pkgs.hunt2025}/lib/hunt2025/dist/static/";
+            locations."/static/".alias = "${pkgs.hunt2025.assets}/static/";
           };
           "things.mitmh2025.com" = {
             forceSSL = true;

infra/modules/hunt2025.nix
@@ -46,6 +46,7 @@ in {
 
       environment.DB_ENV = cfg.db_env;
       environment.PORT = cfg.port;
+      environment.STATIC_PATH = "${pkgs.hunt2025.assets}/static";
       environment.API_BASE_URL = cfg.apiBaseUrl;
       # FIXME: Use a real key in production.
       environment.JWT_SECRET = "%m";

site/default.nix
@@ -3,6 +3,7 @@
 , postgresql
 , playwright-driver
 , buildNpmPackage
+, rsync
 , makeWrapper
 , withDevDeps ? false
 }:
@@ -24,6 +25,7 @@ in buildNpmPackage {
 
   nativeBuildInputs = [
     postgresql
+    rsync
     makeWrapper
   ];
 
@@ -33,8 +35,12 @@ in buildNpmPackage {
 
   dontNpmPrune = if withDevDeps then true else null;
 
+  outputs = [ "out" "assets" ];
+
   postInstall = ''
-    cp -R dist $out/lib/node_modules/hunt2025/dist
+    mkdir -p $assets
+    cp -R dist/static $assets/static
+    rsync -r --exclude static/ dist $out/lib/node_modules/hunt2025/
     mv $out/lib/node_modules/hunt2025 $out/lib/hunt2025
     rmdir $out/lib/node_modules
     makeWrapper ${nodejs}/bin/node $out/bin/hunt2025 \

commit 60c40fb4c6a612da21d89014ad0d320466ad0fd0

Fix lint failure introduced in 5f418c93ad855f9277cf60dcc1de2f66421c99ac

site/src/app.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "fs";
 import path from "path";
 import express from "express";
 import morgan from "morgan";
@@ -8,7 +9,6 @@ import { connect as redisConnect } from "./api/redis";
 import { getRouter } from "./api/server";
 import { getUiRouter } from "./frontend/server/routes";
 import HUNT from "./huntdata";
-import { existsSync } from "fs";
 
 const LOG_FORMAT_DEBUG =
   ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" ":req[Authorization]"';