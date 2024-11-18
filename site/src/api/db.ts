38b08796af120186caee4db6930faafe891bcbe2
Ben Shonaldmann <ben@benweissmann.com>
2024-11-18 13:54:38 -0500

    Back out the NO_RESEED changes

--- site/src/api/db.ts
+++ site/src/api/db.ts
@@ -78,17 +78,13 @@ export async function connect(environment: string) {
     environment === "development" ||
     environment === "ci"
   ) {
-    if (process.env.NO_RESEED && (await knex("teams").pluck("id")).length > 0) {
-      console.log("Skipping reseed");
-    } else {
-      const seedContext = import.meta.webpackContext("../../seeds", {
-        regExp: /^\.\/.*\.ts$/,
-      });
+    const seedContext = import.meta.webpackContext("../../seeds", {
+      regExp: /^\.\/.*\.ts$/,
+    });
 
-      await knex.seed.run({
-        seedSource: new WebpackSeedSource(seedContext),
-      });
-    }
+    await knex.seed.run({
+      seedSource: new WebpackSeedSource(seedContext),
+    });
   }
   return knex;
 }
