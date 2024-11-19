d8381f7208025c46c029b291846135b8f74a772b
Quentin Smith <quentin@mit.edu>
2024-11-19 13:27:53 -0500

    Use a stateful set to run redis so it survives autoscaling

--- infra/terraform/prod/redis.nix
+++ infra/terraform/prod/redis.nix
@@ -33,55 +33,62 @@
       dir /data
     '';
   };
-  resource.kubernetes_persistent_volume_claim_v1.valkey-data = {
-    metadata.namespace = "prod";
-    metadata.name = "valkey-data";
-    spec = {
-      access_modes = ["ReadWriteOnce"];
-      resources.requests.storage = "5Gi";
-      storage_class_name = "standard-rwo";
-    };
-    wait_until_bound = false;
-  };
-  resource.kubernetes_pod_v1.redis = {
+  resource.kubernetes_stateful_set_v1.redis = {
     metadata.namespace = "prod";
     metadata.name = "redis";
     metadata.labels.app = "redis";
-    spec.volume = [
-      {
-        name = "data";
-        persistent_volume_claim.claim_name = lib.tfRef "kubernetes_persistent_volume_claim_v1.valkey-data.metadata[0].name";
-      }
-      {
-        name = "config";
-        config_map.name = lib.tfRef "kubernetes_config_map_v1.valkey.metadata[0].name";
-      }
-      {
-        name = "secret";
-        secret.secret_name = lib.tfRef "kubernetes_secret_v1.valkey.metadata[0].name";
-      }
-    ];
-    spec.container = [{
-      name = "valkey";
-      image = lib.tfRef config.gcp.ar.images.images.valkey.urlRef;
-      args = ["valkey-server" "/config/valkey.conf"];
-      resources = {
-        limits.cpu = "2";
-        limits.memory = "1Gi";
-        requests.cpu = "500m";
-        requests.memory = "250Mi";
+    spec = {
+      replicas = 1;
+      selector.match_labels.app = "redis";
+      service_name = lib.tfRef "kubernetes_service_v1.redis.metadata[0].name";
+      template = {
+        metadata.labels.app = "redis";
+        spec.volume = [
+          {
+            name = "config";
+            config_map.name = lib.tfRef "kubernetes_config_map_v1.valkey.metadata[0].name";
+          }
+          {
+            name = "secret";
+            secret.secret_name = lib.tfRef "kubernetes_secret_v1.valkey.metadata[0].name";
+          }
+        ];
+        spec.container = [{
+          name = "valkey";
+          image = lib.tfRef config.gcp.ar.images.images.valkey.urlRef;
+          args = ["valkey-server" "/config/valkey.conf"];
+          resources = {
+            limits.cpu = "2";
+            limits.memory = "1Gi";
+            requests.cpu = "500m";
+            requests.memory = "250Mi";
+          };
+          liveness_probe = {
+            tcp_socket.port = 6379;
+            initial_delay_seconds = 3;
+            period_seconds = 3;
+          };
+          volume_mount = [
+            { name = "valkey-data"; mount_path = "/data"; }
+            { name = "config"; mount_path = "/config"; read_only = true; }
+            { name = "secret"; mount_path = "/secret"; read_only = true; }
+          ];
+        }];
       };
-      liveness_probe = {
-        tcp_socket.port = 6379;
-        initial_delay_seconds = 3;
-        period_seconds = 3;
+      persistent_volume_claim_retention_policy = {
+        # Redis has no long-lived data that we care about.
+        when_deleted = "Delete";
+        when_scaled = "Delete";
       };
-      volume_mount = [
-        { name = "data"; mount_path = "/data"; }
-        { name = "config"; mount_path = "/config"; read_only = true; }
-        { name = "secret"; mount_path = "/secret"; read_only = true; }
-      ];
-    }];
+      volume_claim_template = [{
+        metadata.name = "valkey-data";
+        spec = {
+          access_modes = ["ReadWriteOnce"];
+          resources.requests.storage = "5Gi";
+          storage_class_name = "standard-rwo";
+        };
+      }];
+    };
   };
   resource.kubernetes_service_v1.redis = {
     metadata.namespace = "prod";
