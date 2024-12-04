e9b1f9ac73e082dc97522f896a083510eecfbf3d
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2024-12-03 22:00:47 -0500

    Update pleasing-racket (Be Mine)

--- site/src/frontend/puzzles/pleasing-racket/index.ts
+++ site/src/frontend/puzzles/pleasing-racket/index.ts
@@ -84,7 +84,13 @@ const puzzle: PuzzleDefinition = {
         "Using the number traced out by the series’ path, index into the first name of the corresponding Valentine character from that series. Order the extracted letters by the color order in which the blank hearts at the bottom of the page appear.",
     },
   ],
-  canned_responses: [],
+  canned_responses: [
+    {
+      guess: ["MAY YOU LOSE ME", "MAY YOU LOVE ME", "SAY YOU LOSE ME"],
+      reply:
+        "Almost there! Make sure to use the name each character is most commonly known by.",
+    },
+  ],
 };
 
 export default puzzle;
