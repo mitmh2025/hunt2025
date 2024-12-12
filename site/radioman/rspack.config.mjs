175103133da1f6b79111202892be97a9026c7600
Quentin Smith <quentin@mit.edu>
2024-12-12 01:56:55 -0500

    Use rspack to build radioman and shrink the image from 1 GiB to 2.2 MiB (!)

---
+++ site/radioman/rspack.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from '@rspack/cli';
+
+const config = defineConfig({
+  entry: {
+    main: './main.ts',
+  },
+  target: 'node22',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        //exclude: [/node_modules/],
+        loader: 'builtin:swc-loader',
+        options: {
+          jsc: {
+            parser: {
+              syntax: 'typescript',
+            },
+          },
+        },
+        type: 'javascript/auto',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    module: true,
+  },
+});
+
+export default config;
