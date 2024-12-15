9cef53c2d2f528f04e2b2f301b5fb6ab7b9f8be1
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2024-12-15 12:19:48 -0500

    Update shameful-duck (Anything Is Popsicle)

--- site/src/frontend/puzzles/shameful-duck/puzzle.tsx
+++ site/src/frontend/puzzles/shameful-duck/puzzle.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { styled } from "styled-components";
 import { type TeamHuntState } from "../../../../lib/api/client";
+import puzzle from "./assets/anything-is-popsicle.pdf";
+
+const StyledIframe = styled.iframe`
+  width: 100%;
+  aspect-ratio: 8.5/11;
+`;
 
 const Puzzle = ({ teamState }: { teamState: TeamHuntState }): JSX.Element => {
   const pickupCompleted =
@@ -14,6 +21,11 @@ const Puzzle = ({ teamState }: { teamState: TeamHuntState }): JSX.Element => {
             You should have received thirty popsicle sticks in a sealed
             cellophane bag.
           </p>
+          <p>
+            The following PDF is provided for accessibility and/or remote
+            solving.
+          </p>
+          <StyledIframe src={puzzle} />
         </>
       ) : (
         <>
