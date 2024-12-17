a74de8ffc74e002343141ffd3f174f54b59eb07b
Evan Broder <evan@ebroder.net>
2024-12-17 15:32:25 -0800

    Shrink cursors to 32x32

--- site/src/frontend/rounds/illegal_search/client/cursors.ts
+++ site/src/frontend/rounds/illegal_search/client/cursors.ts
@@ -7,11 +7,11 @@ import move_right_cursor_image from "../assets/cursors/move-right.png";
 import move_up_cursor_image from "../assets/cursors/move-up.png";
 import zoom_cursor_image from "../assets/cursors/zoom.png";
 
-export const default_cursor = `url('${default_cursor_image}') 28 6, auto`;
-export const draggable_cursor = `url('${draggable_cursor_image}') 32 32, grabbing`;
-export const dragging_cursor = `url('${dragging_cursor_image}') 32 32, grabbing`;
+export const default_cursor = `url('${default_cursor_image}') 14 3, auto`;
+export const draggable_cursor = `url('${draggable_cursor_image}') 16 16, grabbing`;
+export const dragging_cursor = `url('${dragging_cursor_image}') 16 16, grabbing`;
 export const move_up_cursor = `url('${move_up_cursor_image}') 0 0, n-resize`;
 export const move_down_cursor = `url('${move_down_cursor_image}') 0 0, s-resize`;
 export const move_left_cursor = `url('${move_left_cursor_image}') 0 0, w-resize`;
 export const move_right_cursor = `url('${move_right_cursor_image}') 0 0, e-resize`;
-export const zoom_cursor = `url('${zoom_cursor_image}') 18 18, zoom-in`;
+export const zoom_cursor = `url('${zoom_cursor_image}') 9 9, zoom-in`;
