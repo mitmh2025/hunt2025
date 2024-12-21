d406c111f23c4ba7880e0309ca3e5ebae2e2794e
Evan Broder <evan@ebroder.net>
2024-12-20 22:55:58 -0800

    Rework bookcase view
    The original conception of the bookcase dated from when we assumed we
    were going to be going for quasi-photorealistic across the Illegal
    Search. However, given that the rest of our interactable artwork is in a
    more cartoonish style, this reworks the bookcase to be a little more in
    line with that.
    
    Instead of looking at the books straight on, we get a little bit of a
    perspective, so we can see the tops and covers of the books. When they
    are pulled in or out, they slide on an isometric plane instead of just
    getting scaled up and down.

--- site/src/frontend/rounds/illegal_search/client/Bookcase.tsx
+++ site/src/frontend/rounds/illegal_search/client/Bookcase.tsx
@@ -4,14 +4,13 @@ import { type TeamHuntState } from "../../../../../lib/api/client";
 import book_pull from "../assets/bookcase/book_pull.mp3";
 import book_push from "../assets/bookcase/book_push.mp3";
 import dark_wood_texture from "../assets/bookcase/dark_wood_texture.jpg";
-import leather_texture from "../assets/bookcase/leather_texture.png";
 import unlock from "../assets/bookcase/unlock.mp3";
 import { type Node } from "../types";
 import bookcaseData from "./bookcaseData";
 import { draggable_cursor } from "./cursors";
 import playSound from "./playSound";
 
-const BookcaseContainer = styled.table`
+const BookcaseContainer = styled.div`
   width: 840px;
   background-image: url(${dark_wood_texture});
   padding: 20px;
@@ -19,17 +18,19 @@ const BookcaseContainer = styled.table`
   background-color: white;
   position: absolute;
   left: 600px;
+  bottom: 0;
   display: block;
 `;
 
-const BookcaseShelf = styled.tr`
+const BookcaseShelf = styled.div`
+  display: flex;
+  align-items: flex-end;
   position: relative;
   padding-top: 1em;
   background-blend-mode: darken;
   background-color: rgba(0, 0, 0, 0.4);
   box-shadow: rgba(0, 0, 0, 0.5) 0px 5px 10px 0px inset;
   margin-bottom: 10px;
-  display: block;
   width: 800px;
 
   &:last-child {
@@ -37,121 +38,203 @@ const BookcaseShelf = styled.tr`
   }
 `;
 
-const Book = styled.td<{
+const Book = styled.div<{
   $color: string;
   $direction?: string;
   $pulled?: boolean;
   $interactive?: boolean;
 }>`
+  display: flex;
+  flex-direction: column-reverse;
   position: relative;
-  padding: 10px 5px;
-  font-size: 14px;
-  line-height: 1;
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  font-family: Garamond, serif;
-  color: #ffeaca;
-  text-shadow:
-    -0.5px -0.5px rgba(255, 255, 255, 0.3),
-    0.5px 0.5px rgba(0, 0, 0, 0.8);
-  border: 3px inset rgba(0, 0, 0, 0.5);
-  background-image: url(${leather_texture});
-  background-blend-mode: multiply;
-  background-color: ${({ $color }) => $color};
 
-  writing-mode: vertical-rl;
   transition: all 0.2s ease-in-out;
-  transform: rotate(180deg) scale(1);
-  height: 140px;
-  width: 62px;
+  ${({ $pulled }) =>
+    $pulled &&
+    `
+    transform: translate(-10px, 10px);
+  `}
 
-  ${(props) => {
-    if (props.$pulled) {
-      return `
-      transition: all 0.2s ease-in-out;
-      transform: rotate(180deg) scale(1.1); /* Standard */
-      border-radius: 1px;
-      z-index: 10;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    transition: all 0.2s ease-in-out;
+    box-shadow: ${({ $pulled }) =>
+      $pulled ? "0px 17px 16px -11px #fff, 0px -16px 16px -11px #fff" : "none"};
+  }
 
-      &::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        box-shadow: 0px 17px 16px -11px #fff, 0px -16px 16px -11px #fff;
-      }
-    `;
-    }
-    return null;
-  }}
+  & .spine {
+    position: relative;
+    padding: 10px 5px;
+    font-size: 14px;
+    line-height: 1;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-family: Garamond, serif;
+    color: ${({ $color }) => ($color === "#AC950F" ? "#3e2d0c" : "#ffeaca")};
+    text-shadow:
+      -0.5px 0.5px rgba(255, 255, 255, 0.3),
+      0.5px -0.5px rgba(0, 0, 0, 0.8);
+    border: 3px solid black;
 
-  ${(props) => {
-    if (props.$interactive && props.$direction !== "horizontal") {
-      return `
+    background-color: ${({ $color }) => $color};
+    background: linear-gradient(
+        to top,
+        rgba(0, 0, 0, 0) 17%,
+        rgba(255, 255, 255, 0.15) 19%,
+        rgba(0, 0, 0, 0.15) 21%,
+        rgba(0, 0, 0, 0) 23% 37%,
+        rgba(255, 255, 255, 0.15) 39%,
+        rgba(0, 0, 0, 0.15) 41%,
+        rgba(0, 0, 0, 0) 43% 57%,
+        rgba(255, 255, 255, 0.15) 59%,
+        rgba(0, 0, 0, 0.15) 61%,
+        rgba(0, 0, 0, 0) 63% 77%,
+        rgba(255, 255, 255, 0.15) 79%,
+        rgba(0, 0, 0, 0.15) 81%,
+        rgba(0, 0, 0, 0) 83%
+      ),
+      linear-gradient(
+        to top,
+        ${({ $color }) => $color} 0%,
+        ${({ $color }) => $color} 100%
+      );
+
+    writing-mode: vertical-rl;
+    transform: rotate(180deg);
+    height: 140px;
+    width: 62px;
+
+    ${(props) => {
+      if (props.$interactive && props.$direction !== "horizontal") {
+        return `
         &:hover {
           cursor: ${draggable_cursor};
-          z-index: 9;
         }
     `;
-    }
-    return null;
-  }};
+      }
+      return null;
+    }};
 
-  span {
-    display: block;
-    &.author {
-      font-size: 12px;
-      margin-right: 6px;
+    span {
+      display: block;
+      &.author {
+        font-size: 12px;
+        margin-right: 6px;
+      }
     }
+  }
+
+  & .top {
+    position: absolute;
+    background: white;
+    background-image: linear-gradient(90deg, white 90%, gray 10%);
+    background-size: 5px 5px;
+    width: 62px;
+    height: 8px;
+    transform-origin: bottom;
+    transform: translate(0, -140px) skewX(-60deg);
+    border-color: black;
+    border-style: solid;
+    border-width: 3px 3px 0 3px;
+  }
+
+  & .cover {
+    background: ${({ $color }) => $color};
+    width: 14px;
+    height: 140px;
+    position: absolute;
+    transform-origin: left;
+    transform: translate(62px, 0) skewY(-30deg);
+    border-color: black;
+    border-style: solid;
+    border-width: 3px 3px 3px 0;
   }
 `;
 
-const BookcaseShelfExtras = styled.td`
+const BookcaseShelfExtras = styled.div`
+  align-self: stretch;
   display: inline-flex;
   flex-direction: column;
   align-items: flex-end;
-  margin-bottom: -10px;
-  margin-left: 36px;
+  justify-content: flex-end;
+  position: relative;
+  overflow: hidden;
 `;
 
 const HorizontalBook = styled.div<{
   $color: string;
 }>`
-  position: relative;
-  padding: 10px 5px;
-  font-size: 14px;
-  line-height: 1;
-  display: inline-flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  text-align: left;
-  font-family: Garamond, serif;
-  color: #ffeaca;
-  text-shadow:
-    -0.5px -0.5px rgba(255, 255, 255, 0.3),
-    0.5px 0.5px rgba(0, 0, 0, 0.8);
-  border: 3px inset rgba(0, 0, 0, 0.5);
-  background-image: url(${leather_texture});
-  background-blend-mode: multiply;
-  background-color: ${({ $color }) => $color};
+  & .spine {
+    position: relative;
+    padding: 10px 5px;
+    font-size: 14px;
+    line-height: 1;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    text-align: left;
+    font-family: Garamond, serif;
+    color: #ffeaca;
+    text-shadow:
+      0.5px 0.5px rgba(255, 255, 255, 0.3),
+      -0.5px -0.5px rgba(0, 0, 0, 0.8);
+    border: 3px solid black;
+    background-color: ${({ $color }) => $color};
+    background: linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0) 17%,
+        rgba(255, 255, 255, 0.15) 19%,
+        rgba(0, 0, 0, 0.15) 21%,
+        rgba(0, 0, 0, 0) 23% 37%,
+        rgba(255, 255, 255, 0.15) 39%,
+        rgba(0, 0, 0, 0.15) 41%,
+        rgba(0, 0, 0, 0) 43% 57%,
+        rgba(255, 255, 255, 0.15) 59%,
+        rgba(0, 0, 0, 0.15) 61%,
+        rgba(0, 0, 0, 0) 63% 77%,
+        rgba(255, 255, 255, 0.15) 79%,
+        rgba(0, 0, 0, 0.15) 81%,
+        rgba(0, 0, 0, 0) 83%
+      ),
+      linear-gradient(
+        to top,
+        ${({ $color }) => $color} 0%,
+        ${({ $color }) => $color} 100%
+      );
+    z-index: 1;
 
-  width: 140px;
-  height: 62px;
-  padding: 0 5px;
-  border-style: outset;
+    width: 140px;
+    height: 62px;
+    padding: 0 5px;
 
-  span {
-    display: block;
-    &.author {
-      font-size: 12px;
-      margin-top: 6px;
+    span {
+      display: block;
+      &.author {
+        font-size: 12px;
+        margin-top: 6px;
+      }
     }
+  }
+
+  & .cover {
+    position: absolute;
+    width: 140px;
+    height: 14px;
+    background-color: ${({ $color }) => $color};
+    transform-origin: bottom;
+    transform: translate(0, -76px) skewX(-60deg);
+    border-color: black;
+    border-style: solid;
+    border-width: 3px 3px 0 3px;
   }
 `;
 
@@ -177,7 +260,6 @@ export function BookcaseInteraction({
               return (
                 <Book
                   key={j}
-                  style={style}
                   $color={book.color}
                   $pulled={bookState}
                   $interactive={interactive}
@@ -185,27 +267,34 @@ export function BookcaseInteraction({
                     handleClick(i, j);
                   }}
                 >
-                  <span className="title">
-                    {book.title}
-                    <br />
-                  </span>
-                  <span className="author">{book.author}</span>
+                  <div className="spine" style={style}>
+                    <span className="title">
+                      {book.title}
+                      <br />
+                    </span>
+                    <span className="author">{book.author}</span>
+                  </div>
+                  <div className="top" />
+                  <div className="cover" />
                 </Book>
               );
             })}
             {bookcaseData.extraRows[i] && (
               <>
-                <td style={{ width: "0px", display: "inline-flex" }} />
+                <div style={{ flexGrow: 1 }} />
                 <BookcaseShelfExtras>
                   {bookcaseData.extraRows[i]?.map((book, j) => {
                     const style = { fontSize: book.size };
                     return (
                       <HorizontalBook key={j} style={style} $color={book.color}>
-                        <span className="title">
-                          {book.title}
-                          <br />
-                        </span>
-                        <span className="author">{book.author}</span>
+                        <div className="spine">
+                          <span className="title">
+                            {book.title}
+                            <br />
+                          </span>
+                          <span className="author">{book.author}</span>
+                        </div>
+                        <div className="cover" />
                       </HorizontalBook>
                     );
                   })}
