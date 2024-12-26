76a874f3d896961844f12a6ad636f4a8ae400e9f
James Harvey <jmharvey@gmail.com>
2024-12-26 10:02:35 -0800

    Only use high-res wheel image

--- site/src/frontend/puzzles/rundown-spot/puzzle.tsx
+++ site/src/frontend/puzzles/rundown-spot/puzzle.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { styled } from "styled-components";
 import LinkedImage from "../../components/LinkedImage";
 import wheelHi from "./assets/wheel_hi.png";
-import wheelLo from "./assets/wheel_lo.png";
 
 const SizedImage = styled(LinkedImage)`
   width: 624px;
@@ -14,8 +13,7 @@ const Puzzle = () => {
       <p className="puzzle-flavor">Is this 1:1? There’s an outside chance.</p>
       <SizedImage
         alt="A wheel with letters, colors, and symbols"
-        src={wheelLo}
-        fullSizeURL={wheelHi}
+        src={wheelHi}
       />
     </>
   );
