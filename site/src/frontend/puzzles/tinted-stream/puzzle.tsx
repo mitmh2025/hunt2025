56484f7c9df54d5a5227b50f91644ef6ff6c8e83
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2024-12-28 15:46:39 -0500

    Update tinted-stream (Reuse and Recyclability)

--- site/src/frontend/puzzles/tinted-stream/puzzle.tsx
+++ site/src/frontend/puzzles/tinted-stream/puzzle.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { styled } from "styled-components";
 import LinkedImage from "../../components/LinkedImage";
-import genogram1 from "./assets/genogram1.png";
-import genogram2 from "./assets/genogram2.png";
-import genogram3 from "./assets/genogram3.png";
-import genogram4 from "./assets/genogram4.png";
-import genogram5 from "./assets/genogram5.png";
-import genogram6 from "./assets/genogram6.png";
+import genogram1 from "./assets/genogram1.svg";
+import genogram2 from "./assets/genogram2.svg";
+import genogram3 from "./assets/genogram3.svg";
+import genogram4 from "./assets/genogram4.svg";
+import genogram5 from "./assets/genogram5.svg";
+import genogram6 from "./assets/genogram6.svg";
 import grid1 from "./assets/grid1.png";
 import grid10 from "./assets/grid10.png";
 import grid11 from "./assets/grid11.png";
@@ -23,10 +23,6 @@ import grid7 from "./assets/grid7.png";
 import grid8 from "./assets/grid8.png";
 import grid9 from "./assets/grid9.png";
 
-const ImageWrapper = styled.div`
-  margin: 3em 0;
-`;
-
 const FlexWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -41,25 +37,12 @@ const Puzzle = (): JSX.Element => {
         Sometimes she recycles names, other times dresses. Sometimes she hands
         them down to someone else, but that’s another story.
       </p>
-      <ImageWrapper>
-        <LinkedImage src={genogram1} alt="A genogram" />
-      </ImageWrapper>
-      <ImageWrapper>
-        f
-        <LinkedImage src={genogram2} alt="A genogram" />
-      </ImageWrapper>
-      <ImageWrapper>
-        <LinkedImage src={genogram3} alt="A genogram" />
-      </ImageWrapper>
-      <ImageWrapper>
-        <LinkedImage src={genogram4} alt="A genogram" />
-      </ImageWrapper>
-      <ImageWrapper>
-        <LinkedImage src={genogram5} alt="A genogram" />
-      </ImageWrapper>
-      <ImageWrapper>
-        <LinkedImage src={genogram6} alt="A genogram" />
-      </ImageWrapper>
+      <LinkedImage src={genogram1} alt="A genogram" />
+      <LinkedImage src={genogram2} alt="A genogram" />
+      <LinkedImage src={genogram3} alt="A genogram" />
+      <LinkedImage src={genogram4} alt="A genogram" />
+      <LinkedImage src={genogram5} alt="A genogram" />
+      <LinkedImage src={genogram6} alt="A genogram" />
       <p>Enter names left to right, top to bottom.</p>
       <FlexWrapper>
         <img
