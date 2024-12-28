dd7a76e36752da0beef3251397f6c98113f18c87
Drew Fisher <drew.m.fisher@gmail.com>
2024-12-28 01:58:17 -0800

    shoddy-table: break `<code>` anywhere to avoid box overflow

--- site/src/frontend/puzzles/shoddy-table/puzzle.tsx
+++ site/src/frontend/puzzles/shoddy-table/puzzle.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { styled } from "styled-components";
 import spreadsheet from "./assets/10000sheets.xlsx";
+
+const BreakableCode = styled.code`
+  overflow-wrap: anywhere;
+`;
 
 const Puzzle = () => {
   return (
@@ -7,11 +12,11 @@ const Puzzle = () => {
       <p className="puzzle-flavor">Start with what you can see.</p>
 
       <p>
-        <a href={spreadsheet}>10000sheets.xlsx</a> (size: <code>13366337</code>{" "}
-        bytes; sha256{" "}
-        <code>
+        <a href={spreadsheet}>10000sheets.xlsx</a> (size:{" "}
+        <BreakableCode>13366337</BreakableCode> bytes; sha256{" "}
+        <BreakableCode>
           12fa078ffc3c0c7513655d0224f60eddc3078ef3f512a74155c4e949eff94fc9
-        </code>
+        </BreakableCode>
         )
       </p>
     </>
