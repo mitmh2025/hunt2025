ead0367d78b46872e49a49b0dadf25b6106fd0d7
Drew Fisher <drew.m.fisher@gmail.com>
2024-12-28 23:24:31 -0800

    Use HScrollTableWrapper in delirious-mammoth

--- site/src/frontend/puzzles/delirious-mammoth/solution.tsx
+++ site/src/frontend/puzzles/delirious-mammoth/solution.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { styled } from "styled-components";
-import { Mono, PuzzleAnswer } from "../../components/StyledUI";
+import {
+  HScrollTableWrapper,
+  Mono,
+  PuzzleAnswer,
+} from "../../components/StyledUI";
 
 const StyledTable = styled.table`
   margin-bottom: 1em;
@@ -29,181 +33,185 @@ const Solution = (): JSX.Element => {
         <PuzzleAnswer>HEDWIG AND THE ANGRY INCH</PuzzleAnswer>, a Broadway
         musical about a singer.
       </p>
-      <StyledTable>
-        <tr>
-          <th>Order</th>
-          <th>Character that sings tune</th>
-          <th>Character that sings similar lyrics</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>GENIE</td>
-          <td>ANNE BOLEYN</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>GRIZABELLA</td>
-          <td>ANNIE</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>DOLLY</td>
-          <td>EVA</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>JEAN VALJEAN</td>
-          <td>FREDDIE</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>TEVYE</td>
-          <td>MARK</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>ELPHABA</td>
-          <td>PETER PAN</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>FRANKIE</td>
-          <td>SKY</td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td>PHANTOM</td>
-          <td>TRACY</td>
-        </tr>
-        <tr>
-          <td>9</td>
-          <td>ESTHER</td>
-          <td>WITCH</td>
-        </tr>
-      </StyledTable>
+      <HScrollTableWrapper>
+        <StyledTable>
+          <tr>
+            <th>Order</th>
+            <th>Character that sings tune</th>
+            <th>Character that sings similar lyrics</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>GENIE</td>
+            <td>ANNE BOLEYN</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>GRIZABELLA</td>
+            <td>ANNIE</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>DOLLY</td>
+            <td>EVA</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>JEAN VALJEAN</td>
+            <td>FREDDIE</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>TEVYE</td>
+            <td>MARK</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>ELPHABA</td>
+            <td>PETER PAN</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>FRANKIE</td>
+            <td>SKY</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>PHANTOM</td>
+            <td>TRACY</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>ESTHER</td>
+            <td>WITCH</td>
+          </tr>
+        </StyledTable>
+      </HScrollTableWrapper>
       <h3>Appendix</h3>
       <p>
         The songs and shows are all listed below. Each pair has some sort of
         contrast, mostly for the fun of mixing them up.
       </p>
-      <StyledTable>
-        <tr>
-          <th>Order</th>
-          <th>Show</th>
-          <th>(Tune) Song</th>
-          <th>Show</th>
-          <th>(Lyrics) Song</th>
-          <th>Contrast</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>
-            <i>Aladdin</i>
-          </td>
-          <td>Friend Like Me</td>
-          <td>
-            <i>Six</i>
-          </td>
-          <td>Don’t Lose Ur Head</td>
-          <td>Good friend vs. bad friend</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>
-            <i>Cats</i>
-          </td>
-          <td>Memory</td>
-          <td>
-            <i>Annie</i>
-          </td>
-          <td>Tomorrow</td>
-          <td>Thinking of the past vs. the future</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>
-            <i>Hello, Dolly!</i>
-          </td>
-          <td>Hello, Dolly!</td>
-          <td>
-            <i>Evita</i>
-          </td>
-          <td>Don’t Cry for Me Argentina</td>
-          <td>Saying hello vs. goodbye</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>
-            <i>Les Misérables</i>
-          </td>
-          <td>One Day More</td>
-          <td>
-            <i>Chess</i>
-          </td>
-          <td>One Night In Bangkok</td>
-          <td>Day vs. night</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>
-            <i>Fiddler on the Roof</i>
-          </td>
-          <td>If I Were a Rich Man</td>
-          <td>
-            <i>Rent</i>
-          </td>
-          <td>Rent</td>
-          <td>Imagination of wealth vs. Reality of poverty</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>
-            <i>Wicked</i>
-          </td>
-          <td>Defying Gravity</td>
-          <td>
-            <i>Peter Pan</i>
-          </td>
-          <td>I Won’t Grow Up</td>
-          <td>Growing up vs. staying as you are</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>
-            <i>Jagged Little Pill</i>
-          </td>
-          <td>Ironic</td>
-          <td>
-            <i>Guys and Dolls</i>
-          </td>
-          <td>Luck be a Lady</td>
-          <td>Is it ironic or just bad luck?</td>
-        </tr>
-        <tr>
-          <td>8</td>
-          <td>
-            <i>Phantom of the Opera</i>
-          </td>
-          <td>Music of the Night</td>
-          <td>
-            <i>Hairspray</i>
-          </td>
-          <td>You Can’t Stop The Beat</td>
-          <td>Inevitability of music to bring us into dark vs. light</td>
-        </tr>
-        <tr>
-          <td>9</td>
-          <td>
-            <i>Meet Me In St. Louis</i>
-          </td>
-          <td>Have Yourself a Merry Little Christmas</td>
-          <td>
-            <i>Into the Woods</i>
-          </td>
-          <td>Prologue: Into the Woods</td>
-          <td>Blessing vs. cursing</td>
-        </tr>
-      </StyledTable>
+      <HScrollTableWrapper>
+        <StyledTable>
+          <tr>
+            <th>Order</th>
+            <th>Show</th>
+            <th>(Tune) Song</th>
+            <th>Show</th>
+            <th>(Lyrics) Song</th>
+            <th>Contrast</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>
+              <i>Aladdin</i>
+            </td>
+            <td>Friend Like Me</td>
+            <td>
+              <i>Six</i>
+            </td>
+            <td>Don’t Lose Ur Head</td>
+            <td>Good friend vs. bad friend</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>
+              <i>Cats</i>
+            </td>
+            <td>Memory</td>
+            <td>
+              <i>Annie</i>
+            </td>
+            <td>Tomorrow</td>
+            <td>Thinking of the past vs. the future</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>
+              <i>Hello, Dolly!</i>
+            </td>
+            <td>Hello, Dolly!</td>
+            <td>
+              <i>Evita</i>
+            </td>
+            <td>Don’t Cry for Me Argentina</td>
+            <td>Saying hello vs. goodbye</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>
+              <i>Les Misérables</i>
+            </td>
+            <td>One Day More</td>
+            <td>
+              <i>Chess</i>
+            </td>
+            <td>One Night In Bangkok</td>
+            <td>Day vs. night</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>
+              <i>Fiddler on the Roof</i>
+            </td>
+            <td>If I Were a Rich Man</td>
+            <td>
+              <i>Rent</i>
+            </td>
+            <td>Rent</td>
+            <td>Imagination of wealth vs. Reality of poverty</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>
+              <i>Wicked</i>
+            </td>
+            <td>Defying Gravity</td>
+            <td>
+              <i>Peter Pan</i>
+            </td>
+            <td>I Won’t Grow Up</td>
+            <td>Growing up vs. staying as you are</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>
+              <i>Jagged Little Pill</i>
+            </td>
+            <td>Ironic</td>
+            <td>
+              <i>Guys and Dolls</i>
+            </td>
+            <td>Luck be a Lady</td>
+            <td>Is it ironic or just bad luck?</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>
+              <i>Phantom of the Opera</i>
+            </td>
+            <td>Music of the Night</td>
+            <td>
+              <i>Hairspray</i>
+            </td>
+            <td>You Can’t Stop The Beat</td>
+            <td>Inevitability of music to bring us into dark vs. light</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>
+              <i>Meet Me In St. Louis</i>
+            </td>
+            <td>Have Yourself a Merry Little Christmas</td>
+            <td>
+              <i>Into the Woods</i>
+            </td>
+            <td>Prologue: Into the Woods</td>
+            <td>Blessing vs. cursing</td>
+          </tr>
+        </StyledTable>
+      </HScrollTableWrapper>
     </>
   );
 };
