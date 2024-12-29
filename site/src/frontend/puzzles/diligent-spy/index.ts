d827178cd26ee7ee7745c59bbdd5c79f47f1528a
Evan Broder <evan@ebroder.net>
2024-12-29 11:35:33 -0800

    Postprod diligent-spy (Can-Do Transmissions)

--- site/src/frontend/puzzles/diligent-spy/index.ts
+++ site/src/frontend/puzzles/diligent-spy/index.ts
@@ -18,6 +18,7 @@ const puzzle: PuzzleDefinition = {
   additional_credits: [],
   content: {
     component: Puzzle,
+    copyable: false,
   },
   solution: {
     component: Solution,
@@ -26,9 +27,9 @@ const puzzle: PuzzleDefinition = {
     {
       order: 1.0,
       description:
-        "Solvers have listened to the stream and transcribed it, but don't know what to do with the phrases and numbers",
+        "Solvers have listened to the stream and transcribed it, but don’t know what to do with the phrases and numbers",
       nudge:
-        "All of the \u201cstation identifier\u201d phrases can be treated as crossword clues for common words, some of them appearing more than once.",
+        "All of the “station identifier” phrases can be treated as crossword clues for common words, some of them appearing more than once.",
     },
     {
       order: 5.0,
@@ -42,35 +43,35 @@ const puzzle: PuzzleDefinition = {
       description:
         "Solvers have found the first cluephrase, but are having trouble applying it.",
       nudge:
-        "One-time pad ciphers are somewhat unintuitive. The text is broken into 5-digit sequences, but actually needs to be treated as a contiguous string. Letters are all consistently presented as two-digit sequences of numbers, including leading zeroes. And Vigenere/One-Time Pad ciphers use a zero-indexed alphabet, A=00\u2026Z=25. The easiest way to decode is to plug the ciphertext into one of many online Vigenere tools. The one-time pad in this case also matches length with the encoded string, as an additional confirmation that you're on the right track.",
+        "One-time pad ciphers are somewhat unintuitive. The text is broken into 5-digit sequences, but actually needs to be treated as a contiguous string. Letters are all consistently presented as two-digit sequences of numbers, including leading zeroes. And Vigenere/One-Time Pad ciphers use a zero-indexed alphabet, A=00…Z=25. The easiest way to decode is to plug the ciphertext into one of many online Vigenere tools. The one-time pad in this case also matches length with the encoded string, as an additional confirmation that you’re on the right track.",
     },
     {
       order: 20.0,
       description:
         "Solvers have found the second (longer) cluephrase and started following its instructions, but are having trouble getting anything at all.",
       nudge:
-        "When we say \u201cRead static on linear scale\u201d, we are specifically referring to what's called a \u201cspectrogram\u201d of the audio. If you record the audio from your radio, many free tools (we like Audacity) will help you view the spectrogram. It will be on log scale by default, but linear looks a lot better.",
+        "When we say “Read static on linear scale”, we are specifically referring to what’s called a “spectrogram” of the audio. If you record the audio from your radio, many free tools (we like Audacity) will help you view the spectrogram. It will be on log scale by default, but linear looks a lot better.",
     },
     {
       order: 30.0,
       description:
         "Solvers have found the image in the static, but are having trouble interpreting it.",
       nudge:
-        "Make sure you've recorded at least a full loop and are sufficiently zoomed to see the full thing. The exact shape of the image drawn in the spectrogram - including the notching, the 12x80 aspect ratio, and the many small ticks with some holes - should evoke another era-appropriate communications technology.",
+        "Make sure you’ve recorded at least a full loop and are sufficiently zoomed to see the full thing. The exact shape of the image drawn in the spectrogram—including the notching, the 12x80 aspect ratio, and the many small ticks with some holes—should evoke another era-appropriate communications technology.",
     },
     {
       order: 35.0,
       description:
-        "Solvers have read the last clue and still don't know what to look for.",
+        "Solvers have read the last clue and still don’t know what to look for.",
       nudge:
         "This is a punchcard, specifically in the style of an IBM 12-row punch. The wikipedia article about them has some information on how to read a punchcard as letters.",
     },
     {
       order: 40.0,
       description:
-        "Solvers have read out the punchcard but don't have the final answer",
+        "Solvers have read out the punchcard but don’t have the final answer",
       nudge:
-        "The previous cluephrase gave you some instructions about a \u201cnew pad\u201d. Use this to decrypt the punchcard output.",
+        "The previous cluephrase gave you some instructions about a “new pad”. Use this to decrypt the punchcard output.",
     },
   ],
   canned_responses: [
--- site/src/frontend/puzzles/diligent-spy/puzzle.tsx
+++ site/src/frontend/puzzles/diligent-spy/puzzle.tsx
@@ -1,6 +1,24 @@
+import React from "react";
+import { Math, MFrac, MI, MN, MRow } from "../../components/MathML";
+
 import "./assets/numbers.opus";
 import "./assets/static.opus";
 
-const Puzzle = () => null;
+const Puzzle = () => (
+  <p>
+    Scanning late at night, you notice a faint signal from your radio on a
+    MITropolis-based numbers station, PM{" "}
+    <Math>
+      <MFrac>
+        <MRow>
+          <MN>37</MN>
+          <MI>π</MI>{" "}
+        </MRow>
+        <MN>20</MN>
+      </MFrac>
+    </Math>
+    .
+  </p>
+);
 
 export default Puzzle;
