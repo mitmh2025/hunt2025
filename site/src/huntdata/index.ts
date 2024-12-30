c5dd623266bc58de1b9d5e94bfb97750a021a030
James Harvey <jmharvey@gmail.com>
2024-12-30 00:20:04 -0800

    Postprod heavy-iron (Incognito)

--- site/src/huntdata/index.ts
+++ site/src/huntdata/index.ts
@@ -297,7 +297,7 @@ const HUNT: Hunt = {
         { id: "ptp08", unlockable_if: { puzzles_solved: 2 }, unlock_cost: 1, slug: "the_inspectre" },
         { id: "ptp09", unlockable_if: { puzzles_solved: 3 }, unlock_cost: 1, slug: "eponymous_forensic_accountant" },
         { id: "ptp10", unlockable_if: { puzzles_solved: 3 }, unlock_cost: 1, slug: "any_coat_will_do" },
-        { id: "ptp11", unlockable_if: { puzzles_solved: 4 }, unlock_cost: 1 },
+        { id: "ptp11", unlockable_if: { puzzles_solved: 4 }, unlock_cost: 1, slug: "incognito" },
         { id: "ptp12", unlockable_if: { puzzles_solved: 5 }, unlock_cost: 1 },
         { id: "ptp13", unlockable_if: { puzzles_solved: 5 }, unlock_cost: 1 },
         { id: "ptp14", unlockable_if: { puzzles_solved: 6 }, unlock_cost: 1 },
