d4517eb14c5b8ac10fb5c679b18a3dc1a880367b
Quentin Smith <quentin@mit.edu>
2024-12-29 22:52:41 -0500

    Initial telemetry subscription

--- site/radioman/tbutil.ts
+++ site/radioman/tbutil.ts
@@ -1,5 +1,6 @@
 import { Command, Option } from "commander";
 import { Client } from "./tbapi";
+import { type EntityData } from "./tbws";
 
 async function main() {
   const program = new Command();
@@ -49,39 +50,125 @@ async function main() {
 
       const ws = await client.connectWS();
 
-      const initialData = await ws.sendCommand({
-        type: "ENTITY_DATA",
-        query: {
-          entityFilter: {
-            type: "entityList",
-            entityType: "DEVICE",
-            entityList: [entityId],
-          },
-          pageLink: {
-            pageSize: 1000,
-            page: 0,
-          },
-          latestValues: [
-            {
-              type: "TIME_SERIES",
-              key: "fm_frequency",
+      const initialData = await ws.sendCommand(
+        {
+          type: "ENTITY_DATA",
+          query: {
+            entityFilter: {
+              type: "entityList",
+              entityType: "DEVICE",
+              entityList: [entityId],
             },
-          ],
-        },
-        latestCmd: {
-          keys: [
-            {
-              type: "TIME_SERIES",
-              key: "fm_frequency",
+            pageLink: {
+              pageSize: 1000,
+              page: 0,
             },
-          ],
+            latestValues: [
+              {
+                type: "TIME_SERIES",
+                key: "fm_frequency",
+              },
+            ],
+          },
+          latestCmd: {
+            keys: [
+              {
+                type: "TIME_SERIES",
+                key: "fm_frequency",
+              },
+            ],
+          },
         },
-      });
+        (msg) => {
+          console.log("sub update", msg);
+        },
+      );
       console.log("initial data", initialData);
       await new Promise((resolve) => {
         process.once("SIGINT", resolve);
       });
     });
+
+  program
+    .command("radio2sdrangel")
+    .argument("<device-id>")
+    .option("-d, --device <device>", "device index", "0")
+    .option("-c, --channel <channel>", "channel index", "0")
+    .action(
+      async (
+        entityId: string,
+        options: {
+          device: string;
+          channel: string;
+        },
+      ) => {
+        const client = newClient();
+
+        const ws = await client.connectWS();
+
+        console.log("options", options);
+
+        const updateSdrangel = (msg: EntityData) => {
+          (async () => {
+            const str = msg.latest.TIME_SERIES?.fm_frequency?.value;
+            if (!str) {
+              return;
+            }
+            const freq = parseInt(str, 10) * 1000;
+            const deviceSet = (await (
+              await fetch(
+                `http://127.0.0.1:8091/sdrangel/deviceset/${options.device}`,
+              )
+            ).json()) as {
+              channels: {
+                deltaFrequency: number;
+                id: string;
+              }[];
+              samplingDevice: {
+                centerFrequency: number;
+              };
+            };
+            const { centerFrequency } = deviceSet.samplingDevice;
+            const inputFrequencyOffset = freq - centerFrequency;
+            console.log("Tuning to", freq, inputFrequencyOffset);
+            await fetch(
+              `http://127.0.0.1:8091/sdrangel/deviceset/${options.device}/channel/${options.channel}/settings`,
+              {
+                method: "PATCH",
+                body: JSON.stringify({
+                  channelType: "BFMDemod",
+                  direction: 0,
+                  BFMDemodSettings: {
+                    inputFrequencyOffset,
+                  },
+                }),
+              },
+            );
+          })().catch((e: unknown) => {
+            console.log("failed to sync frequency", e);
+          });
+        };
+
+        const initialData = await ws.subscribeTelemetry(
+          {
+            type: "entityList",
+            entityType: "DEVICE",
+            entityList: [entityId],
+          },
+          [
+            {
+              type: "TIME_SERIES",
+              key: "fm_frequency",
+            },
+          ],
+          updateSdrangel,
+        );
+        console.log("initial data", initialData);
+        await new Promise((resolve) => {
+          process.once("SIGINT", resolve);
+        });
+      },
+    );
 
   await program.parseAsync();
 }
--- site/radioman/tbws.ts
+++ site/radioman/tbws.ts
@@ -46,6 +46,8 @@ const EntityIdSchema = z.object({
   entityType: EntityTypeSchema,
 });
 
+export type EntityId = z.infer<typeof EntityIdSchema>;
+
 const EntityFilterSchema = z.discriminatedUnion("type", [
   z.object({
     type: z.literal("singleEntity"),
@@ -62,10 +64,14 @@ const EntityFilterSchema = z.discriminatedUnion("type", [
   }),
 ]);
 
+export type EntityFilter = z.input<typeof EntityFilterSchema>;
+
 const EntityKeySchema = z.object({
   type: EntityKeyTypeSchema,
   key: z.string(),
 });
+
+export type EntityKey = z.input<typeof EntityKeySchema>;
 
 const LatestValueCmdSchema = z.object({
   keys: z.array(EntityKeySchema),
@@ -143,10 +149,12 @@ const ComparisonTsValueSchema = z.object({
 
 const EntityDataSchema = z.object({
   entityId: EntityIdSchema,
-  latest: z.record(z.record(TsValueSchema)),
+  latest: z.record(EntityKeyTypeSchema, z.record(TsValueSchema)),
   timeseries: z.record(z.array(TsValueSchema)).nullable(),
   aggLatest: z.record(z.number(), ComparisonTsValueSchema).nullable(),
 });
+
+export type EntityData = z.infer<typeof EntityDataSchema>;
 
 const EntityDataUpdateMsgSchema = BaseDataUpdateMsgSchema(
   EntityDataSchema,
@@ -182,15 +190,19 @@ type OmitUnion<Type, Keys> = {
   [Property in keyof Type as Exclude<Property, Keys>]: Type[Property];
 };
 
-export class SubscriptionClient {
-  private _ws: WebSocket;
-  private _commands: Map<
-    number,
-    {
+type CommandHandler =
+  | {
       resolve: (result: WsDataMsg) => void;
       reject: (err: Error) => void;
+      sub?: (result: WsDataMsg) => void;
     }
-  >;
+  | {
+      sub: (result: WsDataMsg) => void;
+    };
+
+export class SubscriptionClient {
+  private _ws: WebSocket;
+  private _commands: Map<number, CommandHandler>;
   private _cmdId: number;
 
   constructor(ws: WebSocket) {
@@ -223,32 +235,76 @@ export class SubscriptionClient {
         console.warn(`unexpected reply for cmdId ${cmdId}`);
         return;
       }
-      this._commands.delete(cmdId);
-      const { resolve, reject } = callbacks;
-      if (msg.errorCode === 0) {
-        resolve(msg);
+      if ("resolve" in callbacks) {
+        const { resolve, reject, sub } = callbacks;
+        if (msg.errorCode === 0) {
+          resolve(msg);
+        } else {
+          reject(
+            new ThingsboardError({
+              status: 0,
+              timestamp: 0,
+              errorCode: msg.errorCode,
+              message: msg.errorMsg ?? "",
+            }),
+          );
+        }
+        if (sub) {
+          this._commands.set(cmdId, { sub });
+        }
       } else {
-        reject(
-          new ThingsboardError({
-            status: 0,
-            timestamp: 0,
-            errorCode: msg.errorCode,
-            message: msg.errorMsg ?? "",
-          }),
-        );
+        callbacks.sub(msg);
       }
     }
   }
 
-  async sendCommand(cmd: OmitUnion<WebsocketCmd, "cmdId">) {
+  async sendCommand(
+    cmd: OmitUnion<WebsocketCmd, "cmdId">,
+    onMsg?: (msg: WsDataMsg) => void,
+  ) {
     const cmdId = this._cmdId++;
     return await new Promise((resolve, reject) => {
-      this._commands.set(cmdId, { resolve, reject });
+      this._commands.set(cmdId, { resolve, reject, sub: onMsg });
       const data = JSON.stringify({
         cmds: [{ cmdId, ...cmd }],
       });
       console.log("sent", data);
       this._ws.send(data);
     });
+  }
+
+  async subscribeTelemetry(
+    entityFilter: EntityFilter,
+    keys: EntityKey[],
+    onUpdate: (data: EntityData) => void,
+  ) {
+    const initial = await this.sendCommand(
+      {
+        type: "ENTITY_DATA",
+        query: {
+          entityFilter,
+          pageLink: {
+            pageSize: 1000,
+            page: 0,
+          },
+          latestValues: keys,
+        },
+        latestCmd: {
+          keys,
+        },
+      },
+      (msg: WsDataMsg) => {
+        if (
+          "cmdUpdateType" in msg &&
+          msg.cmdUpdateType === "ENTITY_DATA" && // eslint-disable-line @typescript-eslint/no-unnecessary-condition -- will be more types soon
+          msg.update
+        ) {
+          for (const update of msg.update) {
+            onUpdate(update);
+          }
+        }
+      },
+    );
+    return initial;
   }
 }
