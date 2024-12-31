f97646f26f3b715fa158930fa459065a3a047a6c
Quentin Smith <quentin@mit.edu>
2024-12-31 15:31:09 -0500

    Run a mock OAuth server to mint admin tokens in dev mode

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,4 +1,5 @@
 import { randomBytes } from "node:crypto";
+import { newMockOAuthServer } from "../lib/auth";
 import app from "./app";
 import regsite from "./frontend/regsite/app";
 import "./frontend/server/radio-assets";
@@ -6,17 +7,25 @@ import "./frontend/server/radio-assets";
 const portStr = process.env.PORT ?? "3000";
 const port = isNaN(parseInt(portStr)) ? portStr : parseInt(portStr);
 
+let defaultComponents = "api,ws,ui,reg";
+
+// N.B. process.env.NODE_ENV is compiled by webpack
+const environment = process.env.NODE_ENV ?? "development";
+
+let jwksUri = process.env.JWKS_URI;
+
+if (environment === "development" && jwksUri === undefined) {
+  defaultComponents += ",oauth2";
+}
+
 const enabledComponents = new Set(
-  (process.env.HUNT_COMPONENTS ?? "api,ws,ui,reg").split(","),
+  (process.env.HUNT_COMPONENTS ?? defaultComponents).split(","),
 );
 
 const regsitePortStr = process.env.REGSITE_PORT ?? "3001";
 const regsitePort = isNaN(parseInt(regsitePortStr))
   ? regsitePortStr
   : parseInt(regsitePortStr);
-
-// N.B. process.env.NODE_ENV is compiled by webpack
-const environment = process.env.NODE_ENV ?? "development";
 
 let apiUrl = process.env.API_BASE_URL;
 if (environment === "development" && enabledComponents.has("api") && !apiUrl) {
@@ -33,17 +42,12 @@ if (environment === "development" && !jwtSecret) {
   jwtSecret = randomBytes(128);
 }
 
-const jwksUri = process.env.JWKS_URI;
-
 let frontendApiSecret: string | undefined = process.env.FRONTEND_API_SECRET;
 if (environment === "development" && !frontendApiSecret) {
   frontendApiSecret = randomBytes(16).toString("hex");
   console.log(
     `Generated random frontend API secret for development: ${frontendApiSecret}`,
   );
-}
-if (!frontendApiSecret) {
-  throw new Error("$FRONTEND_API_SECRET not defined in production");
 }
 
 let dataApiSecret: string | undefined = process.env.DATA_API_SECRET;
@@ -65,31 +69,44 @@ const registrationOpen = !process.env.REGISTRATION_CLOSED;
 
 const emailFrom = process.env.EMAIL_FROM;
 
-app({
-  enabledComponents,
-  dbEnvironment,
-  jwtSecret,
-  jwksUri,
-  frontendApiSecret,
-  dataApiSecret,
-  apiUrl,
-  redisUrl,
-  emailFrom,
-})
-  .then((app) =>
+async function main() {
+  if (!frontendApiSecret) {
+    throw new Error("$FRONTEND_API_SECRET not defined in production");
+  }
+  if (environment === "development" && enabledComponents.has("oauth2")) {
+    const oauth = await newMockOAuthServer(3004);
+    const { address, port } = oauth.address();
+    const baseUrl = `http://${address}:${port}`;
+    jwksUri = `${baseUrl}/jwks`;
+    console.log(
+      `Mock OAuth2 server listening on ${baseUrl}/.well-known/openid-configuration`,
+    );
+  }
+  await app({
+    enabledComponents,
+    dbEnvironment,
+    jwtSecret,
+    jwksUri,
+    frontendApiSecret,
+    dataApiSecret,
+    apiUrl,
+    redisUrl,
+    emailFrom,
+  }).then((app) =>
     app.listen(port, () => {
       console.log(`Listening on port ${port}`);
     }),
-  )
-  .catch((err: unknown) => {
-    console.error(err);
-  });
+  );
 
-if (enabledComponents.has("reg") && apiUrl) {
-  regsite({
-    apiUrl,
-    registrationOpen,
-  }).listen(regsitePort, () => {
-    console.log(`Regsite listening on port ${regsitePort}`);
-  });
+  if (enabledComponents.has("reg") && apiUrl) {
+    regsite({
+      apiUrl,
+      registrationOpen,
+    }).listen(regsitePort, () => {
+      console.log(`Regsite listening on port ${regsitePort}`);
+    });
+  }
 }
+main().catch((err: unknown) => {
+  console.error(err);
+});
