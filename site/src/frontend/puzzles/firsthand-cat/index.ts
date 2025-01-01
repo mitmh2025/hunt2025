2e9548522ae1ff15023f38ce5f9082ce6a2f2314
Drew Fisher <drew.m.fisher@gmail.com>
2024-12-31 17:03:17 -0800

    firsthand-cat: address review comments

--- site/src/frontend/puzzles/firsthand-cat/index.ts
+++ site/src/frontend/puzzles/firsthand-cat/index.ts
@@ -26,52 +26,52 @@ const puzzle: PuzzleDefinition = {
     },
     {
       order: 20.0,
-      description: "Solvers can't make sense of the Pokemon names",
+      description: "Solvers can’t make sense of the Pokemon names",
       nudge: "The wild Pokemon are too weak and will need to evolve.",
     },
     {
       order: 21.0,
-      description: "Solvers still can't make sense of the Pokemon names",
+      description: "Solvers still can’t make sense of the Pokemon names",
       nudge: "When a Pokemon evolves, their name changes.",
     },
     {
       order: 22.0,
       description:
-        "Solvers don't understand how the names are supposed to change",
+        "Solvers don’t understand how the names are supposed to change",
       nudge:
-        "Apply the same mechanic used to change a Pokemon's name to another Pokemon's given name to form a chain.",
+        "Apply the same mechanic used to change a Pokemon’s name to another Pokemon’s given name to form a chain.",
     },
     {
       order: 50.0,
       description:
-        "Solvers have identified many Pokemon, but don't understand what to do with the trainers",
+        "Solvers have identified many Pokemon, but don’t understand what to do with the trainers",
       nudge: "There are 13 Pokemon and 13 trainers in total.",
     },
     {
       order: 51.0,
       description:
-        "Solvers still don't understand how the trainers are associated with the Pokemon",
+        "Solvers still don’t understand how the trainers are associated with the Pokemon",
       nudge:
-        "After evolving a Pokemon's name, the new name should match up with one of the trainers' titles (e.g. \"Linguist\")",
+        "After evolving a Pokemon’s name, the new name should match up with one of the trainers’ titles (e.g. “Linguist”)",
     },
     {
       order: 75.0,
       description:
-        "Solvers have matched the Pokemon and trainers but don't know how to extract",
+        "Solvers have matched the Pokemon and trainers but don’t know how to extract",
       nudge:
         "Compare the evolved Pokemon name with the trainer name with a matching title.",
     },
     {
       order: 76.0,
       description:
-        "Solvers still don't understand how to extract from the Pokemon and trainers",
+        "Solvers still don’t understand how to extract from the Pokemon and trainers",
       nudge:
         "The evolved Pokemon name and corresponding trainer names share a letter in the same position.",
     },
     {
       order: 77.0,
       description:
-        "Solvers have extracted letters but don't know what order in which to read them",
+        "Solvers have extracted letters but don’t know what order in which to read them",
       nudge:
         "The Pokemon form a chain via the evolution mechanic. Be sure to start from the Starter pokemon.",
     },
--- site/src/frontend/puzzles/firsthand-cat/solution.tsx
+++ site/src/frontend/puzzles/firsthand-cat/solution.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { styled } from "styled-components";
-import { Mono, PuzzleAnswer } from "../../components/StyledUI";
+import {
+  HScrollTableWrapper,
+  Mono,
+  PuzzleAnswer,
+} from "../../components/StyledUI";
 
 const Table = styled.table`
   border-collapse: collapse;
@@ -27,164 +31,166 @@ const Solution = () => {
         and the name, and ordering by shared pokemon, gives the final answer:{" "}
         <PuzzleAnswer>CHOKING HAZARD</PuzzleAnswer>
       </p>
-      <Table>
-        <thead>
-          <tr>
-            <th>Pokémon 1</th>
-            <th>Pokémon 2</th>
-            <th>Transformation</th>
-            <th>Result</th>
-            <th>Title</th>
-            <th>Name</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>DIAL the Smoliv</td>
-            <td>KHAN the Paras</td>
-            <td>Append ECT to the end</td>
-            <td>
-              <Mono>DIALECT</Mono>
-            </td>
-            <td>Linguist</td>
-            <td>Jessica</td>
-            <td>C</td>
-          </tr>
-          <tr>
-            <td>KHAN the Paras</td>
-            <td>EEPY the Staryu</td>
-            <td>Replace end with phonetic opposite</td>
-            <td>
-              <Mono>COUGH</Mono>
-            </td>
-            <td>Allergist</td>
-            <td>Sarah</td>
-            <td>H</td>
-          </tr>
-          <tr>
-            <td>EEPY the Staryu</td>
-            <td>DRIB the Phanpy</td>
-            <td>Remove PY from the end, add DON to the front</td>
-            <td>
-              <Mono>DONEE</Mono>
-            </td>
-            <td>Beneficiary</td>
-            <td>Sofia</td>
-            <td>O</td>
-          </tr>
-          <tr>
-            <td>DRIB the Phanpy</td>
-            <td>SIN the Ekans</td>
-            <td>Reverse word, take example, reverse, C → K</td>
-            <td>
-              <Mono>WORK</Mono>
-            </td>
-            <td>Laborer</td>
-            <td>Puck</td>
-            <td>K</td>
-          </tr>
-          <tr>
-            <td>SIN the Ekans</td>
-            <td>ANNA the Natu</td>
-            <td>N → X</td>
-            <td>
-              <Mono>SIX</Mono>
-            </td>
-            <td>West End Actress</td>
-            <td>Pip</td>
-            <td>I</td>
-          </tr>
-          <tr>
-            <td>ANNA the Natu</td>
-            <td>GAIT the Girafarig</td>
-            <td>Invert Palindrome</td>
-            <td>
-              <Mono>NAAN</Mono>
-            </td>
-            <td>Breadmaker</td>
-            <td>John</td>
-            <td>N</td>
-          </tr>
-          <tr>
-            <td>GAIT the Girafarig</td>
-            <td>HAIG the Aipom</td>
-            <td>AI → AMBI</td>
-            <td>
-              <Mono>GAMBIT</Mono>
-            </td>
-            <td>Chess Master</td>
-            <td>Glinda</td>
-            <td>G</td>
-          </tr>
-          <tr>
-            <td>HAIG the Aipom</td>
-            <td>PAL the Piplup</td>
-            <td>I → RIN</td>
-            <td>
-              <Mono>HARING</Mono>
-            </td>
-            <td>Street Artist</td>
-            <td>Huxley</td>
-            <td>H</td>
-          </tr>
-          <tr>
-            <td>PAL the Piplup</td>
-            <td>ZAP the Dunsparce</td>
-            <td>Duplicate first Two Letters</td>
-            <td>
-              <Mono>PAPAL</Mono>
-            </td>
-            <td>Cardinal</td>
-            <td>Mario</td>
-            <td>A</td>
-          </tr>
-          <tr>
-            <td>ZAP the Dunsparce</td>
-            <td>OPEN the Kabuto</td>
-            <td>Append PS to the end</td>
-            <td>
-              <Mono>ZAPPS</Mono>
-            </td>
-            <td>Potato Chip Fan</td>
-            <td>Zelda</td>
-            <td>Z</td>
-          </tr>
-          <tr>
-            <td>OPEN the Kabuto</td>
-            <td>JASPER the Abra</td>
-            <td>Second word in magic phrase</td>
-            <td>
-              <Mono>SESAME</Mono>
-            </td>
-            <td>Tahini Vendor</td>
-            <td>Girard</td>
-            <td>A</td>
-          </tr>
-          <tr>
-            <td>JASPER the Abra</td>
-            <td>SMALE the Jigglypuff</td>
-            <td>J → W, P → T</td>
-            <td>
-              <Mono>WASTER</Mono>
-            </td>
-            <td>Slacker</td>
-            <td>Arthur</td>
-            <td>R</td>
-          </tr>
-          <tr>
-            <td>SMALE the Jigglypuff</td>
-            <td>DIAL the Smoliv</td>
-            <td>SM → D, L → LL</td>
-            <td>
-              <Mono>DALLE</Mono>
-            </td>
-            <td>AI Researcher</td>
-            <td>Debra</td>
-            <td>D</td>
-          </tr>
-        </tbody>
-      </Table>
+      <HScrollTableWrapper>
+        <Table>
+          <thead>
+            <tr>
+              <th>Pokémon 1</th>
+              <th>Pokémon 2</th>
+              <th>Transformation</th>
+              <th>Result</th>
+              <th>Title</th>
+              <th>Name</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>DIAL the Smoliv</td>
+              <td>KHAN the Paras</td>
+              <td>Append ECT to the end</td>
+              <td>
+                <Mono>DIALECT</Mono>
+              </td>
+              <td>Linguist</td>
+              <td>Jessica</td>
+              <td>C</td>
+            </tr>
+            <tr>
+              <td>KHAN the Paras</td>
+              <td>EEPY the Staryu</td>
+              <td>Replace end with phonetic opposite</td>
+              <td>
+                <Mono>COUGH</Mono>
+              </td>
+              <td>Allergist</td>
+              <td>Sarah</td>
+              <td>H</td>
+            </tr>
+            <tr>
+              <td>EEPY the Staryu</td>
+              <td>DRIB the Phanpy</td>
+              <td>Remove PY from the end, add DON to the front</td>
+              <td>
+                <Mono>DONEE</Mono>
+              </td>
+              <td>Beneficiary</td>
+              <td>Sofia</td>
+              <td>O</td>
+            </tr>
+            <tr>
+              <td>DRIB the Phanpy</td>
+              <td>SIN the Ekans</td>
+              <td>Reverse word, take example, reverse, C → K</td>
+              <td>
+                <Mono>WORK</Mono>
+              </td>
+              <td>Laborer</td>
+              <td>Puck</td>
+              <td>K</td>
+            </tr>
+            <tr>
+              <td>SIN the Ekans</td>
+              <td>ANNA the Natu</td>
+              <td>N → X</td>
+              <td>
+                <Mono>SIX</Mono>
+              </td>
+              <td>West End Actress</td>
+              <td>Pip</td>
+              <td>I</td>
+            </tr>
+            <tr>
+              <td>ANNA the Natu</td>
+              <td>GAIT the Girafarig</td>
+              <td>Invert Palindrome</td>
+              <td>
+                <Mono>NAAN</Mono>
+              </td>
+              <td>Breadmaker</td>
+              <td>John</td>
+              <td>N</td>
+            </tr>
+            <tr>
+              <td>GAIT the Girafarig</td>
+              <td>HAIG the Aipom</td>
+              <td>AI → AMBI</td>
+              <td>
+                <Mono>GAMBIT</Mono>
+              </td>
+              <td>Chess Master</td>
+              <td>Glinda</td>
+              <td>G</td>
+            </tr>
+            <tr>
+              <td>HAIG the Aipom</td>
+              <td>PAL the Piplup</td>
+              <td>I → RIN</td>
+              <td>
+                <Mono>HARING</Mono>
+              </td>
+              <td>Street Artist</td>
+              <td>Huxley</td>
+              <td>H</td>
+            </tr>
+            <tr>
+              <td>PAL the Piplup</td>
+              <td>ZAP the Dunsparce</td>
+              <td>Duplicate first Two Letters</td>
+              <td>
+                <Mono>PAPAL</Mono>
+              </td>
+              <td>Cardinal</td>
+              <td>Mario</td>
+              <td>A</td>
+            </tr>
+            <tr>
+              <td>ZAP the Dunsparce</td>
+              <td>OPEN the Kabuto</td>
+              <td>Append PS to the end</td>
+              <td>
+                <Mono>ZAPPS</Mono>
+              </td>
+              <td>Potato Chip Fan</td>
+              <td>Zelda</td>
+              <td>Z</td>
+            </tr>
+            <tr>
+              <td>OPEN the Kabuto</td>
+              <td>JASPER the Abra</td>
+              <td>Second word in magic phrase</td>
+              <td>
+                <Mono>SESAME</Mono>
+              </td>
+              <td>Tahini Vendor</td>
+              <td>Girard</td>
+              <td>A</td>
+            </tr>
+            <tr>
+              <td>JASPER the Abra</td>
+              <td>SMALE the Jigglypuff</td>
+              <td>J → W, P → T</td>
+              <td>
+                <Mono>WASTER</Mono>
+              </td>
+              <td>Slacker</td>
+              <td>Arthur</td>
+              <td>R</td>
+            </tr>
+            <tr>
+              <td>SMALE the Jigglypuff</td>
+              <td>DIAL the Smoliv</td>
+              <td>SM → D, L → LL</td>
+              <td>
+                <Mono>DALLE</Mono>
+              </td>
+              <td>AI Researcher</td>
+              <td>Debra</td>
+              <td>D</td>
+            </tr>
+          </tbody>
+        </Table>
+      </HScrollTableWrapper>
     </>
   );
 };
