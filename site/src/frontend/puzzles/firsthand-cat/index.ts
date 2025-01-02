324072d22f833387c590142bd387443cdb5212ec
miscmike <miscmikesyd@gmail.com>
2025-01-02 15:34:56 +1100

    update initial description based off editor feedback

--- site/src/frontend/puzzles/firsthand-cat/index.ts
+++ site/src/frontend/puzzles/firsthand-cat/index.ts
@@ -5,7 +5,7 @@ import Solution from "./solution";
 const puzzle: PuzzleDefinition = {
   title: "Battle Factory",
   slug: "battle_factory",
-  initial_description: "a list of pokemon and opponents",
+  initial_description: "A bunch of Pokemon with names",
   answer: "CHOKING HAZARD",
   authors: ["Dee Ruttenberg"],
   editors: ["Jonathan Lay", "Kevin Hwang", "Sid Creutz"],
