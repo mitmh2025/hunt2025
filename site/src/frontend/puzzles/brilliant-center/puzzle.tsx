90f706f4aba5a8acf7a4f0122d8a6021e3eaf8d7
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2025-01-02 22:47:57 -0500

    Copy-paste support for brilliant-center (Mellow Planet)

--- site/src/frontend/puzzles/brilliant-center/puzzle.tsx
+++ site/src/frontend/puzzles/brilliant-center/puzzle.tsx
@@ -1,6 +1,37 @@
 import React from "react";
 import { styled } from "styled-components";
+import { COPY_ONLY_CLASS } from "../../components/CopyToClipboard";
 import puzzle from "./assets/mellow-planet.pdf";
+
+const DATA: [string, string][] = [
+  ["- .... .. ...", "2024 Olympian Nedoroscik’s specialty, for short"],
+  ["Amnesiac FFVII hero", "An iron one sang ”In-A-Gadda-Da-Vida”"],
+  ["CTRL-C, CTRL-V", "Black and white cat variety"],
+  [
+    "Darla, Angel, or Spike, e.g.",
+    "“I’m A Believer” band member, autocorrected",
+  ],
+  ["Fashion statement for hunters", "Karen or Richard of 70’s pop"],
+  ["Game developer that created Spyro the Dragon", "Linguine’s wider cousin"],
+  ["Japanese wrestling art", "Miami football player"],
+  ["Like a usable air mattress", "Mythical equine visible to virgins"],
+  ["Magnify or reduce an image", "One who perspires"],
+  ["Nasal discharge, to a kindergartener", "Renaissance tenor cornett"],
+  ["Notable clash in a war", "Swashbuckling debtor to heartless Jones"],
+  [
+    "O at the bottom of a letter, perhaps",
+    "Sylvester, Felix, or Garfield, e.g.",
+  ],
+  ["Olaf and Sven’s movie", "TANGY palindromic completer"],
+  ["Pre-workout motion", "Tapestry maker, perhaps"],
+  ["Slasher flick “A ___ on Elm Street”", "This crushes scissors"],
+  ["Wardenclyffe Tower creator", "Tomato in a sewing kit"],
+  ["Will be tense?", "Unwelcome picnic guest"],
+  [
+    "Without permission, as a biography",
+    "You might be wearing a brass one right now",
+  ],
+];
 
 const StyledIframe = styled.iframe`
   width: 100%;
@@ -11,6 +42,17 @@ const Puzzle = (): JSX.Element => {
   return (
     <>
       <StyledIframe src={puzzle} />
+      <p className={`puzzle-flavor ${COPY_ONLY_CLASS}`}>
+        In their eyes, penguins should be at the top of the food chain.
+      </p>
+      <table className={COPY_ONLY_CLASS}>
+        {DATA.map(([left, right], i) => (
+          <tr key={i}>
+            <td>{left}</td>
+            <td>{right}</td>
+          </tr>
+        ))}
+      </table>
     </>
   );
 };
