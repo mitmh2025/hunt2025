67b01486cb329acf693a9f6c5c93226cec5f1c31
miscmike <miscmikesyd@gmail.com>
2025-01-08 00:17:27 +1100

    adjust layout so images are at top level of puzzlecontent DOM

--- site/src/frontend/puzzles/firsthand-cat/puzzle.tsx
+++ site/src/frontend/puzzles/firsthand-cat/puzzle.tsx
@@ -15,32 +15,18 @@ import image11 from "./assets/11.png";
 import image12 from "./assets/12.png";
 import image13 from "./assets/13.png";
 
-const ColumnContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  width: 100%;
-`;
-
-const Item = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-
 const Image = styled(LinkedImage)`
-  width: 160px;
-  height: 160px;
-  object-fit: cover;
-  margin-bottom: 10px;
-  border: 1px solid black;
+  img {
+    width: 160px;
+    height: 160px;
+    margin-bottom: 4px;
+    border: 1px solid black;
+  }
 `;
 
 const Text = styled.p`
   font-size: 16px;
   color: #333;
-  text-align: center;
 `;
 
 const Puzzle = () => {
@@ -63,50 +49,48 @@ const Puzzle = () => {
       <p className="puzzle-flavor">
         Your Pokémon are much too weak to take on your opponents right now!
       </p>
-      <ColumnContainer>
-        <h3>Your Starter:</h3>
-        <Item>
-          <Image src={image01} alt={`Picture 1`} />
-          <Text>DIAL the Smoliv</Text>
-        </Item>
-        <h3>Wild Pokémon: </h3>
-        {items.map((item, index) => (
-          <Item key={index}>
-            <Image src={item.src} alt={`Picture ${index + 2}`} />
-            <Text>{item.text}</Text>
-          </Item>
-        ))}
 
-        <h3>Opponents</h3>
-        <p>
-          AI RESEARCHER DEBRA would like to battle!
-          <br />
-          ALLERGIST SARAH would like to battle!
-          <br />
-          BENEFICIARY SOFIA would like to battle!
-          <br />
-          BREADMAKER JOHN would like to battle!
-          <br />
-          CARDINAL MARIO would like to battle!
-          <br />
-          CHESS MASTER GLINDA would like to battle!
-          <br />
-          LABORER PUCK would like to battle!
-          <br />
-          LINGUIST JESSICA would like to battle!
-          <br />
-          POTATO CHIP FAN ZELDA would like to battle!
-          <br />
-          SLACKER ARTHUR would like to battle!
-          <br />
-          STREET ARTIST HUXLEY would like to battle!
-          <br />
-          TAHINI VENDOR GIRARD would like to battle!
-          <br />
-          WEST END ACTRESS PIP would like to battle!
-          <br />
-        </p>
-      </ColumnContainer>
+      <h3>Your Starter:</h3>
+      <Image src={image01} alt={`Picture 1`} />
+      <Text>DIAL the Smoliv</Text>
+
+      <h3>Wild Pokémon: </h3>
+      {items.map((item, index) => (
+        <>
+          <Image src={item.src} alt={`Picture ${index + 2}`} />
+          <Text>{item.text}</Text>
+        </>
+      ))}
+
+      <h3>Opponents</h3>
+      <p>
+        AI RESEARCHER DEBRA would like to battle!
+        <br />
+        ALLERGIST SARAH would like to battle!
+        <br />
+        BENEFICIARY SOFIA would like to battle!
+        <br />
+        BREADMAKER JOHN would like to battle!
+        <br />
+        CARDINAL MARIO would like to battle!
+        <br />
+        CHESS MASTER GLINDA would like to battle!
+        <br />
+        LABORER PUCK would like to battle!
+        <br />
+        LINGUIST JESSICA would like to battle!
+        <br />
+        POTATO CHIP FAN ZELDA would like to battle!
+        <br />
+        SLACKER ARTHUR would like to battle!
+        <br />
+        STREET ARTIST HUXLEY would like to battle!
+        <br />
+        TAHINI VENDOR GIRARD would like to battle!
+        <br />
+        WEST END ACTRESS PIP would like to battle!
+        <br />
+      </p>
     </>
   );
 };
