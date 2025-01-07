12d4f512dc24a0f8fcafec004129d00c227ae277
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2025-01-07 08:06:56 -0500

    Update sick-crown (Papa's Bookcase Under Blacklight)

--- site/src/frontend/puzzles/sick-crown/index.ts
+++ site/src/frontend/puzzles/sick-crown/index.ts
@@ -33,7 +33,53 @@ const puzzle: PuzzleDefinition = {
   solution: {
     component: Solution,
   },
-  hints: [],
+  hints: [
+    {
+      order: 10.0,
+      description: "Solvers trying to determine what is relevant to meta",
+      keywords: ["Start of meta"],
+      nudge:
+        "The meta involves five puzzles that need to be re-solved producing five new answers under the blacklight.",
+    },
+    {
+      order: 20.0,
+      description:
+        "Solvers have not discovered the newly available clue in the room",
+      nudge: "Have you checked the phone in Papa's office?",
+    },
+    {
+      order: 30.0,
+      description:
+        "Solvers don't know what to do with the four highlighted books",
+      nudge:
+        "The phone could help you understand what to do with these books.  They don't match to feeder answers.",
+    },
+    {
+      order: 50.0,
+      description: "Solvers don't know what the feeders represent",
+      nudge:
+        "The feeder answers all belong to a category of things, and it isn't books.",
+    },
+    {
+      order: 55.0,
+      description: "Solvers still haven't identified feeders",
+      nudge:
+        "The feeders are all movie titles.  The flavor text will help you with the next steps.",
+    },
+    {
+      order: 60.0,
+      description: "Solvers know what feeders represent, but are stuck",
+      nudge:
+        "The flavor text will help you here.  Leads (actors) from the movies did something relating to books...",
+    },
+    {
+      order: 80.0,
+      description:
+        "Solvers have the correct books identified but are stuck at extraction",
+      nudge:
+        "Use the indexing from the telephone hint with the feeder answers.",
+    },
+  ],
   canned_responses: [],
 };
 
