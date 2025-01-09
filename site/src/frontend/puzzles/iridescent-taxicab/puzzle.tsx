be9e13b1c61a947d079fb1698dc734f1ba70fbbd
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2025-01-09 10:36:02 -0500

    Postprod iridescent-taxicab (We Can Do This All Day)

---
+++ site/src/frontend/puzzles/iridescent-taxicab/puzzle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Puzzle = (): JSX.Element => {
+  return (
+    <>
+      Please contact HQ to schedule a judging for the Infinite Scavenger Hunt.
+      You can do so by sending an email to info@mitmh2025.com. Please include
+      your team name in the email.
+    </>
+  );
+};
+
+export default Puzzle;
