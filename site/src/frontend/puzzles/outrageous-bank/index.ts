0ceddbd17b710c598dc85e41fd6d291d19503bdd
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2025-01-10 09:22:17 -0500

    Postprod outrageous-bank (It's All Cheep (And A Sheep) To Me) (#581)
    * Postprod outrageous-bank (It's All Cheep (And A Sheep) To Me)
    
    * smart quotes
    
    * Transcript under the fold

---
+++ site/src/frontend/puzzles/outrageous-bank/index.ts
@@ -0,0 +1,90 @@
+import { type PuzzleDefinition } from "../types";
+import Puzzle from "./puzzle";
+import Solution from "./solution";
+
+const puzzle: PuzzleDefinition = {
+  title: "It’s All Cheep (And A Sheep) To Me",
+  slug: "its_all_cheep_and_a_sheep_to_me",
+  initial_description:
+    "A video featuring cartoon animals with crossword clues written in various languages and bouncy music.",
+  answer: "HEREFORD CATTLE",
+  authors: ["J. Heléne Andersson", "Liz Oppenheim", "Sarah Leadbeater"],
+  editors: ["Joanna Murray", "Kevin Hwang", "Melanie Matchett Wood"],
+  additional_credits: [],
+  content: {
+    component: Puzzle,
+    copyable: true,
+  },
+  solution: {
+    component: Solution,
+  },
+  hints: [
+    {
+      order: 5.0,
+      description: "Getting started",
+      keywords: ["languages"],
+      nudge:
+        "It looks like the captions are written in a bunch of different languages.",
+    },
+    {
+      order: 10.0,
+      description: "Figuring out what the video is referring to",
+      keywords: ["identification", "images", "music"],
+      nudge:
+        "Have you figured out where the music and images come from? They are all from a single set of sources.",
+    },
+    {
+      order: 15.0,
+      description:
+        "If they can’t find a canonical source for the animals sounds in all the languages",
+      keywords: ["canonical source", "animal sounds"],
+      nudge:
+        "The music and images are all from the same set of videos by the same YouTube account. Shazam should be able to help.",
+    },
+    {
+      order: 20.0,
+      description: "If the source material is still elusive",
+      keywords: ["canonical source", "animal sounds", "languages"],
+      nudge:
+        "The music and images can be found in the YouTube account Pulcino Pio.",
+    },
+    {
+      order: 25.0,
+      description: "If they don’t know how to use the video",
+      keywords: ["onomatopoeia", "animal sounds", "linguistic differences"],
+      nudge: "What noise does a cow make in German? How about in French?",
+    },
+    {
+      order: 30.0,
+      description:
+        "If they haven’t figured out how the animal noises are related to the clues",
+      keywords: ["onomatopoeia", "animal sounds"],
+      nudge:
+        "Use the language the clue is written in to find the sound the animal pictured makes",
+    },
+    {
+      order: 65.0,
+      description:
+        "If the clue answers don’t match with the sounds and clues are solved in English",
+      keywords: ["language", "animal sounds"],
+      nudge:
+        "The clues are in different languages. How about the answers to the clues?",
+    },
+    {
+      order: 90.0,
+      description: "extraction",
+      keywords: ["extraction"],
+      nudge:
+        "How is the sound that the animal makes related to the answers to the clues? The sound spelled out in the source video is close to the answer to the clue in the same language.",
+    },
+  ],
+  canned_responses: [
+    {
+      guess: ["MAKE VIDEO IN OTHER LANGUAGE"],
+      reply:
+        "That is correct! Please make a video of the final verse of the song in a language not used in this puzzle (live action with animals would make us extra happy) and send it to us at info@mitmh2025.com. Include your team name and the phrase MAKE VIDEO IN OTHER LANGUAGE.",
+    },
+  ],
+};
+
+export default puzzle;
---
+++ site/src/frontend/puzzles/outrageous-bank/solution.tsx
@@ -0,0 +1,359 @@
+import React from "react";
+import { styled } from "styled-components";
+import {
+  HScrollTableWrapper,
+  Mono,
+  PuzzleAnswer,
+} from "../../components/StyledUI";
+
+const DATA = [
+  ["English", "https://www.youtube.com/watch?v=Mx8s6ZACZFU"],
+  ["French", "https://www.youtube.com/watch?v=6GsPLAslPIA"],
+  ["Romanian", "https://www.youtube.com/watch?v=-zVSIPxJRIw"],
+  ["Catalan", "https://www.youtube.com/watch?v=jOupGCVtLhw"],
+  ["Spanish", "https://www.youtube.com/watch?v=dhsy6epaJGs"],
+  ["Italian", "https://www.youtube.com/watch?v=juqyzgnbspY"],
+  ["German", "https://www.youtube.com/watch?v=tjBCjfB3Hq8"],
+  ["Dutch", "https://www.youtube.com/watch?v=HnWZnrkh3FI"],
+];
+
+const EXTRACTION_TABLE = [
+  [
+    "M",
+    "Romanian",
+    "Goat",
+    "MEEE",
+    "MEME",
+    "O idee care se răspândește prin imitație în cadrul unui grup social",
+    "Meme",
+    "An idea that spreads through imitation within a social group",
+  ],
+  [
+    "A",
+    "Catalan",
+    "Turkey",
+    "GLÚ GLÚ GLÚ",
+    "GLA",
+    "Llavor d’alzina",
+    "Acorn",
+    "Oak seed",
+  ],
+  [
+    "K",
+    "German",
+    "Tractor",
+    "BRUMM",
+    "KRUMM",
+    "Nicht gerade, in Form",
+    "Crooked",
+    "Not straight in shape",
+  ],
+  [
+    "E",
+    "French",
+    "Bull",
+    "MUU",
+    "MUE",
+    "Perte périodique de plumes ou d’une couche externe",
+    "Molt",
+    "Periodic loss of feathers or an outer layer",
+  ],
+  [
+    "V",
+    "Italian",
+    "Lamb",
+    "BEEE",
+    "BEVE",
+    "Cosa si fa quando si ha sete",
+    "Drink",
+    "What one does when one is thirsty",
+  ],
+  [
+    "I",
+    "Romanian",
+    "Pigeon",
+    "GRU GRU",
+    "GRI",
+    "Culoare între alb și negru",
+    "Grey",
+    "Color between white and black",
+  ],
+  [
+    "D",
+    "Spanish",
+    "Cat",
+    "MIAO",
+    "MIDO",
+    "Lo que hago dos veces para cortar una vez",
+    "I measure",
+    "What I do twice to cut once",
+  ],
+  [
+    "E",
+    "Spanish",
+    "Chick",
+    "PIO",
+    "PIE",
+    "Doce pulgadas",
+    "Foot",
+    "Twelve inches",
+  ],
+  [
+    "O",
+    "English",
+    "Goat",
+    "MAH",
+    "MAO",
+    "Founder of the PRC",
+    "Mao",
+    "Founder of the PRC",
+  ],
+  [
+    "I",
+    "Spanish",
+    "Cow",
+    "MOO",
+    "MIO",
+    "Lo que no es tuyo",
+    "Mine",
+    "What is not yours",
+  ],
+  [
+    "N",
+    "Romanian",
+    "Dog",
+    "HAM HAM",
+    "HAN",
+    "Loc pitoresc pentru cazare și mic dejun",
+    "Inn",
+    "Picturesque place for bed and breakfast",
+  ],
+  [
+    "O",
+    "French",
+    "Bull",
+    "MUU",
+    "MOU",
+    "Cède facilement au toucher",
+    "Soft",
+    "Yields easily to touch",
+  ],
+  [
+    "T",
+    "Italian",
+    "Cow",
+    "MOOO",
+    "MOTO",
+    "Termine breve per un potente veicolo a due ruote",
+    "Motorcycle",
+    "Short word for a powerful two-wheeled vehicle",
+  ],
+  [
+    "H",
+    "English",
+    "Turkey",
+    "GOBBLE GOBBLE",
+    "HOBBLE",
+    "Limp",
+    "Hobble",
+    "Limp",
+  ],
+  [
+    "E",
+    "Catalan",
+    "Chick",
+    "PIU",
+    "PEU",
+    "Unitat bàsica de vers",
+    "Foot",
+    "Basic unit of verse",
+  ],
+  [
+    "R",
+    "Dutch",
+    "Tractor",
+    "BROEM",
+    "BROER",
+    "Hij is niet je zus",
+    "Brother",
+    "He is not your sister",
+  ],
+  [
+    "L",
+    "Spanish",
+    "Rooster",
+    "COCOROCO",
+    "COCO LOCO",
+    "Cóctel playero colombiano con tres licores",
+    "Coco Loco",
+    "Colombian beach cocktail with three liquors",
+  ],
+  [
+    "A",
+    "Dutch",
+    "Hen",
+    "TOK TOK",
+    "TAK",
+    "Een deel van een boom of struik",
+    "Branch",
+    "A part of a tree or shrub",
+  ],
+  [
+    "N",
+    "French",
+    "Chick",
+    "PIOU",
+    "PION",
+    "La pièce d’échecs la moins puissante",
+    "Pawn",
+    "The least powerful chess piece",
+  ],
+  [
+    "G",
+    "English",
+    "Cat",
+    "MEW",
+    "MEG",
+    "Nickname for Margaret",
+    "Meg",
+    "Nickname for Margaret",
+  ],
+  [
+    "U",
+    "German",
+    "Pigeon",
+    "GURR",
+    "GURU",
+    "Spirituell Lehrer",
+    "Guru",
+    "Spiritual teacher",
+  ],
+  ["A", "English", "Chick", "CHEEP", "CHEAP", "Tawdry", "Cheap", "Tawdry"],
+  [
+    "G",
+    "Dutch",
+    "Hen",
+    "TOK TOK",
+    "GOK",
+    "Een keuze zonder het juiste antwoord te weten",
+    "Guess",
+    "A choice without knowing the right answer",
+  ],
+  [
+    "E",
+    "Catalan",
+    "Bull",
+    "MUU",
+    "MEU",
+    "Pertànyent a mi",
+    "Mine",
+    "Belonging to me",
+  ],
+];
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  th,
+  td {
+    padding: 0 1em;
+  }
+  th {
+    background-color: var(--teal-300);
+  }
+  tr:nth-child(odd) {
+    background-color: var(--teal-200);
+  }
+  tr:nth-child(even) {
+    background-color: var(--teal-100);
+  }
+`;
+
+const Solution = (): JSX.Element => {
+  return (
+    <>
+      <p>
+        This puzzle references a series of youtube videos known as “Pulcino Pio”
+        (Little Chick Cheep). These videos basically showcase a series of
+        animals and highlight the noises that they make in different languages.
+        For solvers that are not familiar with the songs, an image search of the
+        images should find the videos. Audio identifier programs are also
+        capable of identifying the instrumental version of the song.
+      </p>
+      <p>
+        General Youtube channel:{" "}
+        <a
+          href="https://www.youtube.com/@pulcinopiotv/playlists"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://www.youtube.com/@pulcinopiotv/playlists
+        </a>
+        <ul>
+          {DATA.map(([language, href], i) => (
+            <li key={i}>
+              {language}:{" "}
+              <a href={href} target="_blank" rel="noreferrer">
+                {href}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <p>
+          The puzzle is a short movie that uses footage of the animals from the
+          videos. On top of each animal is a clue phrase written in one of the
+          languages. Solving the clue phrase will give you a word in the same
+          language that is one letter off from the sound that that animal makes
+          in that language (as spelled in the videos). Reading off these letters
+          provides the instruction to <Mono>MAKE VIDEO IN OTHER LANGUAGE</Mono>.
+        </p>
+        <p>
+          When solvers sent in a video of them performing the final chorus of
+          the song in a language not used in this puzzle, they were provided
+          with the final answer, <PuzzleAnswer>HEREFORD CATTLE</PuzzleAnswer>.
+        </p>
+        <HScrollTableWrapper>
+          <StyledTable>
+            <tr>
+              <th></th>
+              <th>Language</th>
+              <th>Animal</th>
+              <th>Video Sound</th>
+              <th>Word</th>
+              <th>Clue</th>
+              <th>Translated Word</th>
+              <th>Translated Clue</th>
+            </tr>
+            {EXTRACTION_TABLE.map(
+              (
+                [
+                  extraction,
+                  language,
+                  animal,
+                  sound,
+                  word,
+                  clue,
+                  translatedWord,
+                  translatedClue,
+                ],
+                i,
+              ) => (
+                <tr key={i}>
+                  <td>{extraction}</td>
+                  <td>{language}</td>
+                  <td>{animal}</td>
+                  <td>{sound}</td>
+                  <td>{word}</td>
+                  <td>{clue}</td>
+                  <td>{translatedWord}</td>
+                  <td>{translatedClue}</td>
+                </tr>
+              ),
+            )}
+          </StyledTable>
+        </HScrollTableWrapper>
+      </p>
+    </>
+  );
+};
+
+export default Solution;
