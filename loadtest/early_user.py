f67297fffad0022c13703544a0b3bfb006a5a740
Ben Shonaldmann <ben@benweissmann.com>
2025-01-10 13:35:17 -0500

    Add websockets to load test

--- loadtest/early_user.py
+++ loadtest/early_user.py
@@ -1,6 +1,12 @@
-from locust import task, run_single_user
+import string
+from locust import task, run_single_user, env
 from locust import FastHttpUser
 import random
+from ws4py.client.geventclient import WebSocketClient
+import gevent
+import time
+
+SOCKETS_PER_USER = 5
 
 
 class early_load_test(FastHttpUser):
@@ -38,13 +44,29 @@ class early_load_test(FastHttpUser):
             catch_response=True,
         ) as resp:
             pass
+
+        cookie_name = "mitmh2025_auth"
+        cookie_value = ""
+        for cookie in self.client.cookiejar:
+            if cookie.name == cookie_name:
+                cookie_value = cookie.value
+                break
+
+        websockets = [
+            WSClient(self.host or "", f"{cookie_name}={cookie_value}", self.environment)
+            for _ in range(SOCKETS_PER_USER)
+        ]
+
+        for ws in websockets:
+            ws.start()
+
         with self.client.request("GET", "/", catch_response=True) as resp:
             pass
         with self.client.request(
             "GET", "/rounds/the_missing_diamond", catch_response=True
         ) as resp:
             for slug, _ in puzzles.items():
-                if f'href="/puzzles/{slug}"' in resp.text:
+                if resp.text and f'href="/puzzles/{slug}"' in resp.text:
                     puzzles[slug] = True
 
         if not puzzles["an_argument"]:
@@ -61,7 +83,7 @@ class early_load_test(FastHttpUser):
         with self.client.request(
             "GET", "/puzzles/an_argument", catch_response=True
         ) as resp:
-            if "Correct!" in resp.text:
+            if resp.text and "Correct!" in resp.text:
                 solved = True
 
         if not solved:
@@ -156,7 +178,7 @@ class early_load_test(FastHttpUser):
         with self.client.request(
             "GET", "/puzzles/zulu_lima", catch_response=True
         ) as resp:
-            if "Correct!" in resp.text:
+            if resp.text and "Correct!" in resp.text:
                 solved = True
 
         if not solved:
@@ -284,6 +306,90 @@ class early_load_test(FastHttpUser):
         with self.client.request("GET", "/", catch_response=True) as resp:
             pass
 
+        for ws in websockets:
+            ws.stop()
+
+
+class WSClient:
+    ws_host: str
+    incoming_greenlet: gevent.Greenlet | None
+    ws: WebSocketClient | None
+    cookie: str
+    environment: env.Environment
+
+    def __init__(self, http_host: str, cookie: str, env: env.Environment):
+        self.ws_host = http_host.replace("http://", "ws://").replace(
+            "https://", "wss://"
+        )
+        self.incoming_greenlet = None
+        self.ws = None
+        self.cookie = cookie
+        self.environment = env
+
+    def start(self):
+        start_perf_counter = time.perf_counter()
+
+        ws = WebSocketClient(self.ws_host + "/ws", headers=[("Cookie", self.cookie)])
+        ws.connect()
+
+        ws.send(
+            '{"rpc":1,"method":"sub","subId":"'
+            + self.randomSubId()
+            + '","dataset":"team_info"}'
+        )
+        ws.send(
+            '{"rpc":2,"method":"sub","subId":"'
+            + self.randomSubId()
+            + '","dataset":"navbar"}'
+        )
+        ws.send(
+            '{"rpc":3,"method":"sub","subId":"'
+            + self.randomSubId()
+            + '","dataset":"activity_log"}'
+        )
+        ws.send(
+            '{"rpc":4,"method":"sub","subId":"'
+            + self.randomSubId()
+            + '","dataset":"hub"}'
+        )
+
+        # wait for a first message
+        first_msg = ws.receive()
+
+        self.environment.events.request.fire(
+            request_type="ws",
+            name="/ws",
+            response_time=(time.perf_counter() - start_perf_counter) * 1000,
+            response_length=len(str(first_msg)),
+            response=None,
+            context=None,
+            exception=None,
+        )
+
+        def incoming():
+            while True:
+                m = ws.receive()
+                if m is not None:
+                    pass
+                else:
+                    break
+
+        self.incoming_greenlet = gevent.spawn(incoming)
+
+        self.ws = ws
+
+    def stop(self):
+        if not self.ws:
+            return
+
+        self.ws.close()
+
+        if self.incoming_greenlet:
+            self.incoming_greenlet.join()
+
+    def randomSubId(self):
+        return "".join(random.choices(string.ascii_letters, k=16))
+
 
 if __name__ == "__main__":
-    run_single_user(early_load_test)
+    run_single_user(early_load_test, include_context=True, loglevel="INFO")
