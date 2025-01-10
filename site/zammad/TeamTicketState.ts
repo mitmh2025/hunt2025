ba66e1e49ec432651069c65303681ec3700da18e
Evan Broder <evan@ebroder.net>
2025-01-10 16:48:15 -0500

    Sync "touchpoint" puzzle state into Zammad
    This rearranges a lot of code to try and keep the sync2zammad file
    itself from being *completely* overwhelming, but in terms of
    functionality it basically adds two new things:
    
    - It periodically polls Zammad for all tickets that have an updated_at
      timestamp newer than the last poll, and tracks that ticket state so we
      have an in-memory cache of what tickets already exist (and so that we
      can trigger when their state changes).
    
    - It tails the activity log, looking for events that satisfy the
      conditions for bartender interactions (now branded "touchpoints") and
      if satisfied, creates an appropriate ticket in Zammad (assuming one
      doesn't already exist).
    
    Still missing from this PR is synchronizing the state of closed tickets
    back into the activity log (where that should resuilt in setting a
    gate). (And, of course, anything involving interactions and hints.)

---
+++ site/zammad/TeamTicketState.ts
@@ -0,0 +1,62 @@
+import { type InternalActivityLogEntry } from "../lib/api/frontend_contract";
+import canonicalizeInput from "../src/api/canonicalizeInput";
+import { TeamStateIntermediate } from "../src/api/logic";
+import { type Hunt } from "../src/huntdata/types";
+import Touchpoints, { type TouchpointSlug } from "./Touchpoints";
+import { type ZammadTicketType } from "./zammadApi";
+
+const activityCreatesTicket = (
+  entry: InternalActivityLogEntry,
+  s: TouchpointSlug,
+): boolean => {
+  const trigger = Touchpoints[s];
+  const { created_if } = trigger;
+  switch (created_if.type) {
+    case "slug_unlocked":
+      return entry.type === "puzzle_unlocked" && entry.slug === created_if.slug;
+    case "slug_partially_solved":
+      return (
+        entry.type === "puzzle_guess_submitted" &&
+        entry.slug === created_if.slug &&
+        canonicalizeInput(entry.data.canonical_input) ===
+          canonicalizeInput(created_if.answer)
+      );
+    // case "gate_satisfied":
+    //   return (
+    //     entry.type === "gate_completed" &&
+    //     entry.slug === created_if.gate &&
+    //     entry.data.puzzle_slug === created_if.puzzle_slug
+    //   );
+    default:
+      created_if satisfies never;
+      return false;
+  }
+};
+
+class TeamTicketState {
+  teamState: TeamStateIntermediate;
+  needTouchpointTickets: Set<TouchpointSlug>;
+  haveTouchpointTickets: Map<TouchpointSlug, ZammadTicketType>;
+
+  constructor(hunt: Hunt, initial?: Partial<TeamTicketState>) {
+    this.teamState = new TeamStateIntermediate(hunt, initial?.teamState);
+    this.needTouchpointTickets = new Set(initial?.needTouchpointTickets ?? []);
+    this.haveTouchpointTickets = new Map(initial?.haveTouchpointTickets ?? []);
+  }
+
+  reduce(entry: InternalActivityLogEntry) {
+    this.teamState.reduce(entry);
+    for (const s of Object.keys(Touchpoints) as TouchpointSlug[]) {
+      if (activityCreatesTicket(entry, s)) {
+        this.needTouchpointTickets.add(s);
+      }
+    }
+
+    // haveTouchpointTickets is populated by Zammad ticket changes, not by the
+    // activity log.
+
+    return this;
+  }
+}
+
+export default TeamTicketState;
