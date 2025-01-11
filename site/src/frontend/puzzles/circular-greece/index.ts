3705448f21c3ae463b1e695dab10fc5bed6d566c
Arcturus Wang <arcturus@boxaroo.me>
2025-01-10 20:35:29 -0500

    Update site/src/frontend/puzzles/circular-greece/index.ts
    Co-authored-by: Evan Broder <evan@ebroder.net>

--- site/src/frontend/puzzles/circular-greece/index.ts
+++ site/src/frontend/puzzles/circular-greece/index.ts
@@ -5,7 +5,7 @@ import Solution from "./solution";
 const puzzle: PuzzleDefinition = {
   title: "Educational Rite of Passage",
   slug: "educational_rite_of_passage",
-  initial_description: "Physical Puzzle—45 3d-printed pieces and a stick",
+  initial_description: "Physical Puzzle—45 3D-printed pieces and a stick",
   answer: "RAW ANTIQUE BRASS",
   authors: ["Laura Nicholson", "Arcturus Wang", "Robert “Fro” Myers"],
   editors: ["Elan Blaustein", "Michele Pratusevich", "Nathan Fung"],
