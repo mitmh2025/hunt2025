6f718efbfc0a7f1860079d88fff7111230cd2f81
Quentin Smith <quentin@mit.edu>
2025-01-14 15:30:50 -0500

    Run sync2zammad in production

--- infra/terraform/prod/tix.nix
+++ infra/terraform/prod/tix.nix
@@ -12,4 +12,29 @@
     useSops = true;
     nixosConfiguration = self.nixosConfigurations."prod/tix";
   };
+
+  sops.keys.tix = {};
+  data.sops_file.tix.source_file = "${../../secrets/prod/tix.yaml}";
+
+  k8s.prod.statefulSet.sync2zammad = {
+    image = lib.tfRef config.gcp.ar.images.images.misc.urlRef;
+    env = {
+      API_BASE_URL = "http://api/api";
+      ZAMMAD_URL = "https://tix.mitmh2025.com";
+    };
+    secretEnv = {
+      FRONTEND_API_SECRET = lib.tfRef "random_password.frontend_api_secret.result";
+      REDIS_URL = ''redis://default:${lib.tfRef "random_password.valkey.result"}@redis'';
+      ZAMMAD_SECRET = lib.tfRef ''data.sops_file.tix.data["zammad.sync2zammad_token"]'';
+    };
+    container = {
+      args = ["sync2zammad"];
+      resources = {
+        limits.cpu = "1.5";
+        limits.memory = "1Gi";
+        requests.cpu = "200m";
+        requests.memory = "250Mi";
+      };
+    };
+  };
 }
