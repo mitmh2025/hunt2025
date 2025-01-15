034fa8ad0d086cea871032f2cac46e0b8b158575
Ben Shonaldmann <ben@benweissmann.com>
2025-01-15 17:38:48 -0500

    Increase fudge factor and dont log currentTime requests

--- site/src/frontend/interactions/constants.ts
+++ site/src/frontend/interactions/constants.ts
@@ -4,4 +4,4 @@ export const INTERACTION_START_TIMEOUT_MSEC = 120 * 1000;
 
 // How much time should we allow for propagation delay at each node in the interaction graph, so
 // that we (ideally) don't cut off any audio lines before they finish playing?
-export const PROPAGATION_DELAY_FUDGE_FACTOR_MSEC = 300;
+export const PROPAGATION_DELAY_FUDGE_FACTOR_MSEC = 600;
