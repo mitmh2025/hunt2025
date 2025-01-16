9cdb0e995e6190cc6f8196260cfafdf78e1db3ea
als364 <ariel.leah.schwartz@gmail.com>
2025-01-16 17:50:43 -0500

    Embed videos for four interactions

--- site/src/frontend/interactions/confront_gladys/interaction.tsx
+++ site/src/frontend/interactions/confront_gladys/interaction.tsx
@@ -34,7 +34,19 @@ const Interaction = ({
         </p>
       </AuthorsNoteBlock>
 
-      {interactionState.state === "completed" && <p>TODO: video goes here</p>}
+      {interactionState.state === "completed" && (
+        <p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/U0wNKdgCnFg?si=FhwsxnHgr8kWthnb"
+            title="Confronting Gladys"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </p>
+      )}
     </>
   );
 };
