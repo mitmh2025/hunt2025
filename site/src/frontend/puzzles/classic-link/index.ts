67e591cf8d9e1cd45edfa808fd1ca31997a1fce9
Evan Broder <evan@ebroder.net>
2025-01-16 13:34:57 -0500

    Update partial with correct phrase

--- site/src/frontend/puzzles/classic-link/index.ts
+++ site/src/frontend/puzzles/classic-link/index.ts
@@ -67,7 +67,7 @@ const puzzle: PuzzleDefinition = {
     {
       guess: ["GIVE US DOUGH"],
       reply:
-        "Bring us dough—but fully baked, please!  Bring the real thing to the bartenders at the Gala, who will give you the puzzle answer. The bartenders are a bit snobbish, so homemade items will get you some bonus points. If you’re willing, please also take a picture of your baked good and send it along with the recipe to info@mitmh2025.com to enter your submission into the Gala Bake-off. Include your team name and the phrase BRING US DOUGH in the subject line.",
+        "Bring us dough—but fully baked, please!  Bring the real thing to the bartenders at the Gala, who will give you the puzzle answer. The bartenders are a bit snobbish, so homemade items will get you some bonus points. If you’re willing, please also take a picture of your baked good and send it along with the recipe to info@mitmh2025.com to enter your submission into the Gala Bake-off. Include your team name and the phrase GIVE US DOUGH in the subject line.",
       providesSolveReward: true,
     },
   ],
