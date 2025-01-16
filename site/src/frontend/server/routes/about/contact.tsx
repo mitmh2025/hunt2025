52a1985ea09ad704d18a81690927655cc8a20a72
Ariel Schwartz <ariel.leah.schwartz@gmail.com>
2025-01-15 19:47:37 -0500

    Stub Contact HQ page (#789)

---
+++ site/src/frontend/server/routes/about/contact.tsx
@@ -0,0 +1,58 @@
+import { type Request } from "express";
+import React from "react";
+import { wrapContentWithNavBar } from "../../../components/ContentWithNavBar";
+import {
+  PageWrapper,
+  PageHeader,
+  PageTitle,
+  PageMain,
+} from "../../../components/PageLayout";
+
+export function contactHandler(req: Request) {
+  const teamState = req.teamState;
+  if (teamState === undefined) return undefined;
+
+  const node = (
+    <PageWrapper>
+      <>
+        <PageHeader>
+          <PageTitle>Contact HQ</PageTitle>
+        </PageHeader>
+        <PageMain>
+          <p>
+            There are lots of great reasons to visit or contact us—if a puzzle
+            instructs you to, if you think there is an erratum or technical
+            issue, if you are not having fun and want extra help, or if you just
+            want to say hi!
+          </p>
+          <p>
+            If you need to reach out, the easiest option is to visit us at the
+            Gala bar. The bar staff can direct any questions to the right
+            people. The second best option is email, at{" "}
+            <a
+              href="mailto:info@mitmh2025.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              info@mitmh2025.com
+            </a>
+            . In an urgent situation, you can also call us at 617-324-7732.
+          </p>
+          <p>
+            The Gala is located in the Stata Center 4th Floor R&amp;D Commons.
+            Detailed instructions for accessing Stata and the Gala can be found{" "}
+            <a href="/about#the-gala">here</a>.
+          </p>
+        </PageMain>
+      </>
+    </PageWrapper>
+  );
+
+  return wrapContentWithNavBar(
+    {
+      node,
+      title: "Contact HQ",
+    },
+    teamState,
+  );
+}
