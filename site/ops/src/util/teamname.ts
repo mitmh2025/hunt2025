45b5ff8dbcfcf8c4fcd7e0001e4548fdc904c702
Hubert Hwang <hubert.y.hwang@gmail.com>
2025-01-16 20:29:41 -0500

    truncating both team name and username to 15 characters

--- site/ops/src/util/teamname.ts
+++ site/ops/src/util/teamname.ts
@@ -2,12 +2,18 @@ import { getTeamName } from "../../../src/utils/teamNames";
 import { type TeamData } from "../opsdata/types";
 
 export function abbreviatedName(team: TeamData, includeUsername: boolean) {
+  console.log(team);
+  console.log(team.username);
   const teamName = getTeamName(team.username);
   const displayName =
-    teamName.length <= 20 ? teamName : `${teamName.slice(0, 20)}...`;
+    teamName.length <= 15 ? teamName : `${teamName.slice(0, 15)}...`;
+  const displayUsername =
+    team.username.length <= 15
+      ? team.username
+      : `${team.username.slice(0, 15)}...`;
   if (!includeUsername) {
     return displayName;
   } else {
-    return `${displayName} (${team.username})`;
+    return `${displayName} (${displayUsername})`;
   }
 }
