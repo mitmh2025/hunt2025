b1923529affb9d25d16ef3a5f9b60fd29f662b3d
Drew Fisher <drew.m.fisher@gmail.com>
2025-01-19 00:38:58 -0500

    Add errata for Given Up to direct people away from 32-123
    Apparently the room is in use and the occupants are not thrilled that
    people keep coming in.

--- site/src/frontend/puzzles/giant-switch/puzzle.tsx
+++ site/src/frontend/puzzles/giant-switch/puzzle.tsx
@@ -14,6 +14,8 @@ import "./assets/interrupt-987.opus";
 import "./assets/interrupt-1011.opus";
 import "./assets/interrupt-1051.opus";
 import "./assets/interrupt-1063.opus";
+import { AuthorsNote } from "../../components/PuzzleLayout";
+import { Errata } from "../../components/StyledUI";
 // Used presentationally
 import img1 from "./assets/img1.jpg";
 import img10 from "./assets/img10.svg";
@@ -34,12 +36,27 @@ const StyledImage = styled(LinkedImage)`
 const Puzzle = () => {
   return (
     <>
+      <Errata
+        errata={[
+          {
+            timestamp: "January 19th, at 12:50 AM",
+            message:
+              "The sixth location pictured here is unexpectedly inaccessible this weekend.  You may be able to retrieve the relevant information from outside its basement entrance. Please do not disrupt the (non puzzle-related) occupants of this room.",
+          },
+        ]}
+      />
       <StyledImage src={img1} alt="A ceiling" />
       <StyledImage src={img2} alt="A ceiling" />
       <StyledImage src={img3} alt="A ceiling" />
       <StyledImage src={img4} alt="A ceiling" />
       <StyledImage src={img5} alt="A ceiling" />
       <StyledImage src={img6} alt="A ceiling" />
+      <AuthorsNote>
+        We apologize that the above location is unexpectedly inaccessible this
+        weekend. You may be able to retrieve the relevant information from
+        outside its basement entrance. Please do not disrupt the (non
+        puzzle-related) occupants of this room.
+      </AuthorsNote>
       <StyledImage src={img7} alt="A ceiling" />
       <StyledImage src={img8} alt="A ceiling" />
       <StyledImage src={img9} alt="A ceiling" />
