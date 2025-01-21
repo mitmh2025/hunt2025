63cd093f4a0e6ecee24f300d3b5b17defefc9e46
Arcturus Wang <arcturus@boxaroo.me>
2025-01-21 15:02:15 -0500

    archival version and credits for clean-ivory

--- site/src/frontend/puzzles/clean-ivory/puzzle.tsx
+++ site/src/frontend/puzzles/clean-ivory/puzzle.tsx
@@ -1,45 +1,50 @@
 import React from "react";
-import type { TeamHuntState } from "../../../../lib/api/client";
-import { ArchivalNotice } from "../../components/PuzzleLayout";
-import { MailtoLink } from "../../components/StyledUI";
+import { AuthorsNoteBlock } from "../../components/PuzzleLayout";
+import jigsaw from "./assets/the-inspectre-jigsaw.svg";
 import paper from "./assets/the-inspectre.pdf";
 
-const Puzzle = ({ teamState }: { teamState: TeamHuntState }): JSX.Element => {
-  const pickupCompleted =
-    teamState.rounds.paper_trail?.gates?.includes("ptg02") ?? false;
-
+const Puzzle = (): JSX.Element => {
   return (
     <>
-      <ArchivalNotice />
-      {pickupCompleted ? (
-        <>
-          <p>Our records show you have picked up your copy of this puzzle.</p>
-          <p>
-            You should have received a bag of eight larger acrylic pieces, 228
-            smaller ones, and a sheet of paper. Please contact us at{" "}
-            <MailtoLink subject={"Missing pieces for The Inspectre"} /> if it
-            seems that you are missing pieces.
-          </p>
-          <p>
-            The paper should have{" "}
-            <a href={paper} target="_blank" rel="noreferrer">
-              this
-            </a>{" "}
-            printed on it. If printing this yourself, please ensure that you
-            print at 100% scale.
-          </p>
-        </>
-      ) : (
-        <>
-          <p>Please come to the Gala to pick up your copy of this puzzle.</p>
-          <p>
-            You should receive a bag of eight larger acrylic pieces, 228 smaller
-            ones, and a sheet of paper. Please contact us at{" "}
-            <MailtoLink subject={"Missing pieces for The Inspectre"} /> if it
-            seems that you are missing pieces.
-          </p>
-        </>
-      )}
+      <AuthorsNoteBlock>
+        <p>
+          During Mystery Hunt, teams were instructed to visit us at the Gala to
+          pick up the puzzle. When they did so, they received a paper mat and a
+          bag of pinkish-red transparent acrylic jigsaw pieces.{" "}
+        </p>
+
+        <p>
+          The jigsaw was cut on a lasercutter from 1/16” transparent fluorescent
+          red{" "}
+          <a
+            href="https://houstonacrylic.com/collections/1-16-acrylic/products/1-16-fluorescent-red-pink-cast-acrylic-sheet"
+            target="_blank"
+            rel="noreferrer"
+          >
+            acrylic
+          </a>
+          ; any thickness/shade of transparent acrylic can be used, although
+          we’d recommend 1/8” or thinner and a non-clear color for keeping track
+          of pieces. The SVG for the jigsaw can be found{" "}
+          <a href={jigsaw} target="_blank" rel="noreferrer">
+            here
+          </a>
+          , with cut lines in red and score lines in blue. When scaling, please
+          note that the bounding box is 11.75”x18.75”. Note that this svg
+          contains the fully-assembled jigsaw, but once removed from the
+          lasercutter bed we don’t expect it to impact the fun or challenge of
+          assembling the pieces.
+        </p>
+
+        <p>
+          The underlying mat is{" "}
+          <a href={paper} target="_blank" rel="noreferrer">
+            here
+          </a>{" "}
+          and was printed on 13”x19” cardstock; if printing at home, it can be
+          split into 4 letter-sized chunks and taped together carefully.
+        </p>
+      </AuthorsNoteBlock>
     </>
   );
 };
