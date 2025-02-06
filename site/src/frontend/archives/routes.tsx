398ecc2a722e4a70ac6f3d8dad564fbd3575a8e8
Evan Broder <evan@ebroder.net>
2025-02-06 13:59:45 -0800

    Add site skeleton for archives
    This introduces a router that gets mounted at /2025/ before the main
    hunt router, giving us a place to put archive-specific content. It adds
    a landing page (with a summary of the hunt) along with a placeholder
    stats page, just as a demo of the structure.

---
+++ site/src/frontend/archives/routes.tsx
@@ -0,0 +1,24 @@
+import expressAsyncHandler from "express-async-handler";
+import { Router } from "websocket-express";
+import renderApp from "../utils/renderApp";
+import { statsHandler } from "./stats";
+import { indexHandler } from ".";
+
+export function getArchiveRouter() {
+  const router = new Router();
+  router.get(
+    "/",
+    expressAsyncHandler(async (req, res, next) => {
+      await renderApp(indexHandler, req, res, next);
+    }),
+  );
+
+  router.get(
+    "/extras/stats",
+    expressAsyncHandler(async (req, res, next) => {
+      await renderApp(statsHandler, req, res, next);
+    }),
+  );
+
+  return router;
+}
