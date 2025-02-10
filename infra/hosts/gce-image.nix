241d0e40ee6983ab631aea58a632247233a9ffe6
Quentin Smith <quentin@mit.edu>
2025-02-10 04:58:11 -0500

    Note command for reporting host keys

--- infra/hosts/gce-image.nix
+++ infra/hosts/gce-image.nix
@@ -4,4 +4,7 @@
     ./base.nix
     "${modulesPath}/virtualisation/google-compute-image.nix"
   ];
+
+  # TODO: Report ssh host keys at startup
+  # curl -X PUT --data @<(awk '{ print $2 }' /etc/ssh/ssh_host_ed25519_key.pub) http://metadata.google.internal/computeMetadata/v1/instance/guest-attributes/hostkeys/ssh-ed25519 -H "Metadata-Flavor: Google"
 }
