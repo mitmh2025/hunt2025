5a6a3e6a632973648d91850d8a21cbe4096cfeac
Quentin Smith <quentin@mit.edu>
2025-03-01 16:11:09 -0500

    Properly wait for DHCP + DNS before running activation from initrd

--- infra/services/gce-vm.nix
+++ infra/services/gce-vm.nix
@@ -22,11 +22,20 @@
       boot.initrd.network.enable = true;
       networking.useNetworkd = true;
       security.polkit.enable = true; # Allows networkd to set hostname
-      boot.initrd.systemd.services.initrd-nixos-activation = let
-        deps = [ "network-online.target" ];
-      in {
-        after = deps;
-        wants = deps;
+      # Wait for a DHCP lease before we reach initrd.target
+      # See https://www.freedesktop.org/software/systemd/man/latest/bootup.html
+      boot.initrd.systemd.targets.initrd = {
+        after = ["systemd-network-wait-online@eth0.service"];
+        wants = ["systemd-network-wait-online@eth0.service"];
+      };
+      # initrd-cleanup.service will stop resolved before initrd-nixos-activation (which includes setupSecrets) starts, so we need to make sure /etc/resolv.conf doesn't contain resolved's stub resolver.
+      # N.B. For some reason, we end up with 1.1.1.1/8.8.8.8 instead of the DHCP DNS servers, but that still seems to work.
+      boot.initrd.systemd = {
+        contents."/etc/systemd/resolved.conf".text = lib.mkForce ''
+          [Resolve]
+          DNSStubListener=no
+        '';
+        tmpfiles.settings.systemd-resolved-stub."/etc/resolv.conf".L.argument = lib.mkForce "/run/systemd/resolve/resolv.conf";
       };
       system.nssDatabases.hosts = lib.mkOrder 401 ["myhostname"]; # Ensure local hostname is not resolved via DNS
     }
