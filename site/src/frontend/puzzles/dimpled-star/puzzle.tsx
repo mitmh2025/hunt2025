d6944addd211be2f92ab5415e04edcc40a2854bf
Evan Broder <evan@ebroder.net>
2025-04-14 15:27:02 -0400

    Update The Thief for archive mode

--- site/src/frontend/puzzles/dimpled-star/puzzle.tsx
+++ site/src/frontend/puzzles/dimpled-star/puzzle.tsx
@@ -32,10 +32,10 @@ import "./assets/waypoint9.opus";
 import type { TeamHuntState } from "../../../../lib/api/client";
 import { Math, MFrac, MI, MN, MRow } from "../../components/MathML";
 import {
+  AuthorsNote,
   AuthorsNoteBlock,
   InteractionLinkBlock,
 } from "../../components/PuzzleLayout";
-import { MailtoLink } from "../../components/StyledUI";
 import rootUrl from "../../utils/rootUrl";
 
 const Note = styled.p`
@@ -44,16 +44,7 @@ const Note = styled.p`
   font-family: "Reenie Beanie";
 `;
 
-const Puzzle = ({
-  teamName,
-  teamState,
-}: {
-  teamName: string;
-  teamState: TeamHuntState;
-}) => {
-  const videoReleased =
-    teamState.rounds.missing_diamond?.gates?.includes("mdg12") ?? false;
-
+const Puzzle = ({ teamState }: { teamState: TeamHuntState }) => {
   const interactionState =
     teamState.rounds.missing_diamond?.interactions?.the_crime_scene?.state;
 
@@ -72,84 +63,86 @@ const Puzzle = ({
 
       <AuthorsNoteBlock>
         <p>
-          Send 2-4 investigators with no mobility or hearing impairments to the
-          Gala, and tell a bartender that you’re looking for Billie. Your
-          investigators will need your team’s radio. They will need to be able
-          to do quite a bit of walking, but very little of it outside. They may
-          be out of your office for an hour or so with the radio, so remember to
-          tune to 2π in your browser.
-        </p>
-
-        <p>
-          If you cannot do this on-campus activity due to campus availability or
-          accessibility concerns, or you are stuck or not having fun, you may
-          contact HQ if this puzzle has been open for at least 3 hours and we
-          will arrange an alternative for you. To do this, send an email to{" "}
-          <MailtoLink subject={"THE FINAL WITNESS"} /> with the subject line
-          “THE FINAL WITNESS” (that’s not an answer, it just helps us with
-          dispatch). However, we strongly recommend you experience it in person
-          if possible.
+          During Mystery Hunt, teams were told to send 2-4 investigators with no
+          mobility or hearing impairments to the Gala, and tell a bartender that
+          they were looking for Billie. They were told that investigators would
+          need your team’s radio, and that they would need to be able to do
+          quite a bit of walking, but very little of it outside. Teams were also
+          reminded to tune to 2π in their browser while the investigators were
+          out of the office with the radio.
         </p>
       </AuthorsNoteBlock>
 
       <p className="puzzle-flavor">How should we confront the thief?</p>
 
-      {videoReleased && (
-        <>
-          <hr />
-          <p>
-            When you arrived at the Gala, a paralegal met you with a note from
-            Billie:
-          </p>
-          <Note>
-            <i>Dear {teamName}:</i>
-            <br />
-            <i>
-              I need your help with something. There’s one more witness we need
-              to track down. But there’s too many open ears in this place, so
-              let’s meet somewhere a little more private. First things first,
-              give my associate that radio.
-            </i>
-          </Note>
-          <p>
-            The paralegal then took your radio and tuned it to{" "}
-            <Math>
-              <MFrac>
-                <MRow>
-                  <MN>17</MN>
-                  <MI>π</MI>
-                </MRow>
-                <MRow>
-                  <MN>10</MN>
-                </MRow>
-              </MFrac>
-            </Math>{" "}
-            before returning it to you and continuing to read:
-          </p>
-          <Note>
-            <i>
-              Make a note, kid; this is the best station for catching crooks. I
-              have some last minute business to take care of nearby, but head
-              down those stairs and out the door and I’ll meet you there in a
-              minute.
-            </i>
-          </Note>
-          <p>
-            You then left down the stairs to find Billie, but when you got to
-            the bottom of the staircase, Billie’s voice came to you over the
-            radio:
-          </p>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/9XjTJp2fDPQ?"
-            title="The Runaround"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </>
-      )}
+      <hr />
+
+      <AuthorsNoteBlock>
+        <p>
+          When teams arrived at the Gala, a bartender informed them that Billie
+          had left a note behind <em>specifically</em> for them, and proceeded
+          to read it exactly as written:
+        </p>
+      </AuthorsNoteBlock>
+
+      <Note>
+        <i>Dear [insert team name here]:</i>
+        <br />
+        <i>
+          I need your help with something. There’s one more witness we need to
+          track down. But there’s too many open ears in this place, so let’s
+          meet somewhere a little more private. First things first, give my
+          associate that radio.
+        </i>
+      </Note>
+      <AuthorsNote>
+        At this point, the bartender took the radio and tuned it to{" "}
+        <Math>
+          <MFrac>
+            <MRow>
+              <MN>17</MN>
+              <MI>π</MI>
+            </MRow>
+            <MRow>
+              <MN>10</MN>
+            </MRow>
+          </MFrac>
+        </Math>{" "}
+        before returning it and continuing to read:
+      </AuthorsNote>
+      <Note>
+        <i>
+          Make a note, kid; this is the best station for catching crooks. I have
+          some last minute business to take care of nearby, but head down those
+          stairs and out the door and I’ll meet you there in a minute.
+        </i>
+      </Note>
+
+      <AuthorsNoteBlock>
+        <p>
+          After this briefing, teams left down the stairs out of the Gala to
+          find Billie. However, when they reached the bottom of the staircase,
+          Billie’s voice suddenly began speaking to them through their radio. As
+          they followed Billie around campus via the narration, they would
+          receive additional instruction via radio voiceover.
+        </p>
+
+        <p>
+          The following video shows the path around campus, with the voiceover
+          that would have come from the radio. During Mystery Hunt, it was be
+          released to teams on request once at least 3 hours had passed since
+          the puzzle was unlocked.
+        </p>
+      </AuthorsNoteBlock>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/9XjTJp2fDPQ?"
+        title="The Runaround"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </>
   );
 };
