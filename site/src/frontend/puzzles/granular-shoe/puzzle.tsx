8b3804be058cfe99571eacf7db0b2e7622378cdf
Evan Broder <evan@ebroder.net>
2025-04-24 22:06:28 -0700

    Add postsolve for granular-shoe

--- site/src/frontend/puzzles/granular-shoe/puzzle.tsx
+++ site/src/frontend/puzzles/granular-shoe/puzzle.tsx
@@ -1,51 +1,48 @@
-import React from "react";
-import type { TeamHuntState } from "../../../../lib/api/client";
-import { ArchivalNotice } from "../../components/PuzzleLayout";
-import { MailtoLink } from "../../components/StyledUI";
+import { AuthorsNoteBlock } from "../../components/PuzzleLayout";
+import art from "./assets/art.pdf";
+import magnetsCricut from "./assets/magnets-cricut.png";
+import magnetsPDF from "./assets/magnets.pdf";
 
-const Puzzle = ({ teamState }: { teamState: TeamHuntState }): JSX.Element => {
-  const pickupCompleted =
-    teamState.rounds.murder_in_mitropolis?.gates?.includes("tmg04");
-
+const Puzzle = (): JSX.Element => {
   return (
     <>
-      <ArchivalNotice />
+      <AuthorsNoteBlock>
+        <p>
+          This puzzle was provided as an envelope containing 116 pieces of
+          (regular) paper and 40 magnets printed on magnetic sheets.
+        </p>
 
-      {pickupCompleted ? (
-        <>
-          <p>Our records show you have picked up your copy of this puzzle.</p>
-          <p>
-            You should have received a sealed envelope containing 116 pieces of
-            paper and 40 magnets. (Any pieces of paper with nothing but a large
-            X on both sides were included by mistake, are not part of the puzzle
-            (including counting towards the 116 pieces), and should be
-            discarded.) Please contact us at{" "}
-            <MailtoLink
-              subject={
-                "Missing pieces for abstract art and poems / concerning a pale blue dot / and many more friends"
-              }
-            />{" "}
-            if it seems that you are missing pieces.
-          </p>
-        </>
-      ) : (
-        <>
-          <p>Please come to the Gala to pick up your copy of this puzzle.</p>
-          <p>
-            You should receive a sealed envelope containing 116 pieces of paper
-            and 40 magnets. (Any pieces of paper with nothing but a large X on
-            both sides were included by mistake, are not part of the puzzle
-            (including counting towards the 116 pieces), and should be
-            discarded.) Please contact us at{" "}
-            <MailtoLink
-              subject={
-                "Missing pieces for abstract art and poems / concerning a pale blue dot / and many more friends"
-              }
-            />{" "}
-            if it seems that you are missing pieces.
-          </p>
-        </>
-      )}
+        <p>
+          If you’d like to try and solve it at home, you can print out the{" "}
+          <a href={art} target="_blank" rel="noreferrer">
+            paper
+          </a>{" "}
+          double-sided, flipping over the long edge, then cut into strips along
+          the black outlines. You can also print out the “
+          <a href={magnetsPDF} target="_blank" rel="noreferrer">
+            magnets
+          </a>
+          ”, and then cut them out along their outlines (you should end up with
+          40 clusters of words). These don‘t need to be printed out on magnetic
+          sheets, although you certainly can, but if you do, make sure the
+          sheets you use are appropriate for your printer.
+        </p>
+
+        <p>
+          If you’re making a single copy of this puzzle, even if you want to
+          strive for authenticity by using magnetic sheets, we would strongly
+          recommend cutting out the magnets by hand. However, if you really love
+          your Cricut (or other cutting plotter), we’ve included the{" "}
+          <a href={magnetsCricut} target="_blank" rel="noreferrer">
+            magnets in the format used to produce the puzzle
+          </a>
+          , which is squeezed together to fit on a Cricut cutting bed and
+          formatted for import into Cricut Design Space. We will warn you that
+          in our experience, producing the magnets using a Cricut did save
+          direct human labor compared to cutting them out by hand, but did not
+          save time.
+        </p>
+      </AuthorsNoteBlock>
     </>
   );
 };
