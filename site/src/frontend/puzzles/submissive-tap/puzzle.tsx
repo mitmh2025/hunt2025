3b165c7f237b4715a85b9abaf170114823d2e3dc
Evan Broder <evan@ebroder.net>
2025-06-09 16:59:51 -0700

    Archival version of submissive-tap

--- site/src/frontend/puzzles/submissive-tap/puzzle.tsx
+++ site/src/frontend/puzzles/submissive-tap/puzzle.tsx
@@ -1,34 +1,56 @@
 import React from "react";
-import type { TeamHuntState } from "../../../../lib/api/client";
+import { AuthorsNoteBlock } from "../../components/PuzzleLayout";
 
-export default function Puzzle({ teamState }: { teamState: TeamHuntState }) {
-  const eventConcluded = teamState.rounds.events?.gates?.includes("evg01");
-
+export default function Puzzle() {
   return (
     <>
       <p className="puzzle-flavor">
         That was a lot of effort to get one piece of information, wasn’t it?
       </p>
-      {eventConcluded && (
-        <>
-          <p>
-            For your convenience, this is a recording of what all teams that
-            participated in the event live should have encountered. All teams
-            received the same content.
-          </p>
-          <p>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/Ka2V--_PxXw"
-              title="Tailing a Lead"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-          </p>
-        </>
-      )}
+      <AuthorsNoteBlock>
+        <p>
+          For this event, teams had to follow a suspect around MITropolis. The
+          suspect turned out to be quite loose-lipped when they thought nobody
+          was around, and teams were provided with a set of instructions for how
+          to render themselves invisible, at least as far as the suspect could
+          tell. Teams had to follow the suspect around, remain hidden, and learn
+          what they could from the suspect’s conversations.
+        </p>
+
+        <p>
+          In this video, you can watch as one set of teams follows their lead
+          (mostly successfully). When all was said and done, teams could use the
+          information they had gathered to come up with the answer for this
+          event; there should be enough information in the video for you to
+          figure it out as well, so the video doesn’t spell out the answer for
+          you.
+        </p>
+
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dHqM3H2vmHg"
+          title="Tailing a Lead (Recap) - MITMH2025"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+
+        <p>
+          (During Mystery Hunt, we provided a more streamlined and less
+          informative video for teams who were participating remotely. You will
+          likely find it easier to solve the puzzle using our updated video
+          above, but you can also still{" "}
+          <a
+            href="https://www.youtube.com/watch?v=Ka2V--_PxXw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            watch the original
+          </a>{" "}
+          if you’d like.)
+        </p>
+      </AuthorsNoteBlock>
     </>
   );
 }
