90e58905843eab0ba0e1fece831449d0ea2a42a8
Evan Broder <evan@ebroder.net>
2025-06-20 21:08:20 -0700

    Update archive homepage

--- site/src/frontend/archives/index.tsx
+++ site/src/frontend/archives/index.tsx
@@ -29,15 +29,12 @@ export function indexHandler() {
             storied <strong>Shadow Diamond</strong>, is discovered to be
             missing.
           </p>
-
           <p>
             Papa hires private eye Billie O’Ryan, of the Two P.I. Noir Detective
             Agency, who in turn recruits teams to join them and help find the
             diamond.
           </p>
-
           <h2>Prequel</h2>
-
           <p>
             In October,{" "}
             <strong>
@@ -50,14 +47,11 @@ export function indexHandler() {
             is a prequel to the story of The Case of the Shadow Diamond, where
             Papa organizes a bank robbery to steal a valuable rare coin.
           </p>
-
           <h2>Invite</h2>
-
           <p>
             At the beginning of January, teams (with a US mailing address)
             received an invitation in the mail from Papa to the Gala:
           </p>
-
           <p style={{ display: "flex", maxWidth: "500px", margin: "0 auto" }}>
             <LinkedImage
               src={inviteFront}
@@ -65,9 +59,7 @@ export function indexHandler() {
               fullSizeURL={invitePDF}
             />
           </p>
-
           <h2>Kickoff</h2>
-
           <p>
             <strong>
               <a href="https://www.youtube.com/watch?v=cWPZ61YgY6s">
@@ -76,7 +68,6 @@ export function indexHandler() {
             </strong>{" "}
             on YouTube.
           </p>
-
           <p>
             The 2025 MIT Mystery Hunt was kicked off on Friday, January 17th at
             noon. It was presented in three different classrooms as a broadcast
@@ -86,16 +77,13 @@ export function indexHandler() {
             O’Ryan to find the diamond, starting by investigating those closest
             to it.
           </p>
-
           <h2>The Hunt</h2>
-
           <p>
             <strong>
               View the <a href="/2025/hunt/">Hunt website</a>
             </strong>{" "}
             as seen by teams.
           </p>
-
           <p>
             In typical noir fashion, while searching for the missing diamond,
             teams learn that the situation is far more complicated than it
@@ -104,16 +92,13 @@ export function indexHandler() {
             to uncover. The thief who stole the diamond turns up dead, and the
             diamond they had stolen turns out to be fake.
           </p>
-
           <p>
             After finding the thief’s killer and learning everyone’s secrets,
             Billie and teams had to break into the Finster family vault, after
             which they learned that someone unexpected had been behind the whole
             thing…
           </p>
-
           <h2>Winning Team</h2>
-
           <p>
             At 8:56 AM on Sunday, January 19th, <strong>Cardinality</strong>{" "}
             became the first team to solve their final major investigation. At
@@ -124,14 +109,16 @@ export function indexHandler() {
             stolen by Papa, but instead they received a stash of surprisingly
             coin-like counterfeit jewelry manufactured by one of the characters.
           </p>
-
+          <p>
+            A video showing footage of Cardinality breaking into the vault and
+            learning the dramatic end of the story is available on the page for{" "}
+            <a href={`${rootUrl}/interactions/the_vault`}>The Vault</a>.
+          </p>
           <p>
             Between when Cardinality finished and wrap-up, an additional 10
             teams (for 11 total) managed to finish the Hunt.
           </p>
-
           <h2>Wrap-Up</h2>
-
           <p>
             A wrap-up event for the Hunt was held at noon on Monday.{" "}
             <a href="https://www.youtube.com/watch?v=2qwjMkvlAyI">
@@ -139,26 +126,33 @@ export function indexHandler() {
             </a>{" "}
             on YouTube.
           </p>
-
           <h2>Extras</h2>
-
           <p>
             The <a href={rootUrl}>Hunt website</a> allows you to re-play the
             2025 MIT Mystery Hunt with an in-browser re-implementation of the
-            Hunt’s unlock structure. Experience the Hunt as teams did, or skip
-            to the end to see all of the puzzles and content. We’ve also made at
-            least minor — and occasionally significant — modifications to nearly
-            25% of the puzzles and interactions. We believe this should allow
-            you to solve (or, in a few cases, at least experience) those puzzles
-            which previously required physical components, in-person elements,
-            or live staffing.
+            Hunt’s unlock structure. Experience the Hunt as teams did, or choose
+            to unlock all puzzles to see all of the puzzles and content. We’ve
+            also made at least minor — and occasionally significant —
+            modifications to nearly 25% of the puzzles and interactions. We
+            believe this should allow you to solve (or, in a few cases, at least
+            experience) those puzzles which previously required physical
+            components, in-person elements, or live staffing.
           </p>
-
+          <p>
+            We’ve also collated video footage taken during Hunt into recaps of{" "}
+            <a href={`${rootUrl}/rounds/events`}>several of the events</a>,{" "}
+            <a href={`${rootUrl}/interactions/the_vault`}>the finale</a>, and a
+            few particularly dramatic puzzles such as{" "}
+            <a href={`${rootUrl}/puzzles/control_room`}>Control Room</a> and{" "}
+            <a href={`${rootUrl}/puzzles/in_communicado_tonight`}>
+              In Communicado Tonight
+            </a>
+            .
+          </p>
           <p>
             Outside of the Hunt website, we also have a few pages of bonus
             content, analysis, and behind-the-scenes coverage:
           </p>
-
           <ul>
             <li>
               <a href="/2025/extras/coin">Coin</a>: A detailed look at the
